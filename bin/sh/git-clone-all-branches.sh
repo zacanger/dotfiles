474bd0fa489de4ff0a9c5474596bae2432196c6e
zacanger <zac@zacanger.com>
2016-01-04 22:20:15 -0700

    foo

---
+++ bin/sh/git-clone-all-branches.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+git branch -a | grep -v HEAD | perl -ne 'chomp($_); s|^\*?\s*||; if (m|(.+)/(.+)| && not $d{$2}) {print qq(git branch --track $2 $1/$2\n)} else {$d{$_}=1}' | csh -xfs
