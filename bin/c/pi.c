8b89fc55c2be485edd430cd01b9a0d82ce92e8c2
zacanger <zac@zacanger.com>
2016-01-11 16:15:47 -0700

    foo

--- bin/c/pi.c
+++ bin/c/pi.c
@@ -1,7 +1,8 @@
-//Why does this print (an approximation to) pi?
+/* Why does this print (an approximation to) pi? */
 
 #include <stdio.h>
 int main(){
+  printf("why does this print (an approximation to) pi? \n");
   unsigned s=1,p=0,k,n=1<<16;
   while(k=s%n*(s%n),s++)p+=(k+s/n*(s/n)>=k);
   printf("%f\n",4.0*p/n/n);
