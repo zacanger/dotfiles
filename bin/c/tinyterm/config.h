0f8e64287d300955834a2b1244021b47a1f1ed9f
zacanger <zac@zacanger.com>
2016-01-12 08:08:09 -0700

    tt

--- bin/c/tinyterm/config.h
+++ bin/c/tinyterm/config.h
@@ -1,99 +1,96 @@
-// Ryan P.C. McQuen | Everett, WA | ryan.q@linux.com
+/* emulation ($TERM) */
+#define TT_TERMINFO               "xterm-256color"
 
-/* Terminal emulation (value of $TERM) (default: xterm) */
-#define TINYTERM_TERMINFO               "xterm-256color"
+//#define TT_SCROLLBAR_VISIBLE      // uncomment to show scrollbar
+#define TT_DYNAMIC_WINDOW_TITLE   // uncomment to enable window_title_cb
+//#define TT_URGENT_ON_BELL         // uncomment to enable window_urgency_hint_cb
+#define TT_URL_BLOCK_MOUSE        // uncomment to block mouse (button-press events) in url-select mode
+#define TT_SCROLLBACK_LINES       10000
+#define TT_SEARCH_WRAP_AROUND     TRUE
+#define TT_AUDIBLE_BELL           FALSE
+#define TT_VISIBLE_BELL           FALSE
+#define TT_FONT                   "Fira Code 10"
+#define TT_X_WINDOW_SIZE          600
+#define TT_Y_WINDOW_SIZE          400
 
-//#define TINYTERM_SCROLLBAR_VISIBLE      // uncomment to show scrollbar
+/* VTE_ANTI_ALIAS_USE_DEFAULT, VTE_ANTI_ALIAS_FORCE_ENABLE, VTE_ANTI_ALIAS_FORCE_DISABLE */
+#define TT_ANTIALIAS      VTE_ANTI_ALIAS_FORCE_ENABLE
 
-#define TINYTERM_DYNAMIC_WINDOW_TITLE   // uncomment to enable window_title_cb
-#define TINYTERM_URGENT_ON_BELL         // uncomment to enable window_urgency_hint_cb
-#define TINYTERM_URL_BLOCK_MOUSE        // uncomment to block mouse (button-press events) in url-select mode
-#define TINYTERM_SCROLLBACK_LINES       15000
-#define TINYTERM_SEARCH_WRAP_AROUND     TRUE
-#define TINYTERM_AUDIBLE_BELL           FALSE
-#define TINYTERM_VISIBLE_BELL           FALSE
-#define TINYTERM_FONT                   "Hasklig 10"
-#define TINYTERM_X_WINDOW_SIZE          600
-#define TINYTERM_Y_WINDOW_SIZE          400
+/* VTE_CURSOR_SHAPE_BLOCK, VTE_CURSOR_SHAPE_IBEAM, VTE_CURSOR_SHAPE_UNDERLINE */
+#define TT_CURSOR_SHAPE   VTE_CURSOR_SHAPE_UNDERLINE
 
-/* One of VTE_ANTI_ALIAS_USE_DEFAULT, VTE_ANTI_ALIAS_FORCE_ENABLE, VTE_ANTI_ALIAS_FORCE_DISABLE */
-#define TINYTERM_ANTIALIAS      VTE_ANTI_ALIAS_FORCE_ENABLE
+/* VTE_CURSOR_BLINK_SYSTEM, VTE_CURSOR_BLINK_ON, VTE_CURSOR_BLINK_OFF */
+#define TT_CURSOR_BLINK   VTE_CURSOR_BLINK_ON
 
-/* One of VTE_CURSOR_SHAPE_BLOCK, VTE_CURSOR_SHAPE_IBEAM, VTE_CURSOR_SHAPE_UNDERLINE */
-#define TINYTERM_CURSOR_SHAPE   VTE_CURSOR_SHAPE_UNDERLINE
+/* double-click selection */
+#define TT_WORD_CHARS "-A-Za-z0-9:./?%&#_=+@~"
 
-/* One of VTE_CURSOR_BLINK_SYSTEM, VTE_CURSOR_BLINK_ON, VTE_CURSOR_BLINK_OFF */
-#define TINYTERM_CURSOR_BLINK   VTE_CURSOR_BLINK_ON
-
-/* Selection behavior for double-clicks */
-#define TINYTERM_WORD_CHARS "-A-Za-z0-9:./?%&#_=+@~"
-
-/* Custom color scheme */
-#define TINYTERM_COLOR_BACKGROUND   "#000000"
-#define TINYTERM_COLOR_FOREGROUND   "#bebebe"
+/* colours */
+#define TT_COLOR_BACKGROUND   "#101010"
+#define TT_COLOR_FOREGROUND   "#cdcdcd"
 /* black */
-#define TINYTERM_COLOR0     "#000000"
-#define TINYTERM_COLOR8     "#4d4d4d"
+#define TT_COLOR0     "101010"
+#define TT_COLOR8     "#4d4d4d"
 /* red */
-#define TINYTERM_COLOR1     "#B22222"
-#define TINYTERM_COLOR9     "#ED2939"
+#define TT_COLOR1     "#B22222"
+#define TT_COLOR9     "#ED2939"
 /* green */
-#define TINYTERM_COLOR2     "#00a000"
-#define TINYTERM_COLOR10    "#32cd32"
+#define TT_COLOR2     "#00a000"
+#define TT_COLOR10    "#32cd32"
 /* yellow */
-#define TINYTERM_COLOR3     "#cdcd00"
-#define TINYTERM_COLOR11    "#ffff00"
+#define TT_COLOR3     "#cdcd00"
+#define TT_COLOR11    "#ffff00"
 /* blue */
-#define TINYTERM_COLOR4     "#2346AE"
-#define TINYTERM_COLOR12    "#2b65ec"
+#define TT_COLOR4     "#2346AE"
+#define TT_COLOR12    "#2b65ec"
 /* magenta */
-#define TINYTERM_COLOR5     "#AA00AA"
-#define TINYTERM_COLOR13    "#C154C1"
+#define TT_COLOR5     "#AA00AA"
+#define TT_COLOR13    "#C154C1"
 /* cyan */
-#define TINYTERM_COLOR6     "#58C6ED"
-#define TINYTERM_COLOR14    "#00DFFF"
+#define TT_COLOR6     "#58C6ED"
+#define TT_COLOR14    "#00DFFF"
 /* white */
-#define TINYTERM_COLOR7     "#e5e5e5"
-#define TINYTERM_COLOR15    "#ffffff"
+#define TT_COLOR7     "#e5e5e5"
+#define TT_COLOR15    "#dddddd"
 
-///* Default color scheme - matches default colors of urxvt */
-//#define TINYTERM_COLOR_BACKGROUND   "#000000"
-//#define TINYTERM_COLOR_FOREGROUND   "#bebebe"
+///* default colours; like urxvt */
+//#define TT_COLOR_BACKGROUND   "#000000"
+//#define TT_COLOR_FOREGROUND   "#bebebe"
 ///* black */
-//#define TINYTERM_COLOR0      "#000000"
-//#define TINYTERM_COLOR8      "#4d4d4d"
+//#define TT_COLOR0      "#000000"
+//#define TT_COLOR8      "#4d4d4d"
 ///* red */
-//#define TINYTERM_COLOR1      "#cd0000"
-//#define TINYTERM_COLOR9      "#ff0000"
+//#define TT_COLOR1      "#cd0000"
+//#define TT_COLOR9      "#ff0000"
 ///* green */
-//#define TINYTERM_COLOR2      "#00cd00"
-//#define TINYTERM_COLOR10     "#00ff00"
+//#define TT_COLOR2      "#00cd00"
+//#define TT_COLOR10     "#00ff00"
 ///* yellow */
-//#define TINYTERM_COLOR3      "#cdcd00"
-//#define TINYTERM_COLOR11     "#ffff00"
+//#define TT_COLOR3      "#cdcd00"
+//#define TT_COLOR11     "#ffff00"
 ///* blue */
-//#define TINYTERM_COLOR4      "#0000cd"
-//#define TINYTERM_COLOR12     "#2b65ec"
+//#define TT_COLOR4      "#0000cd"
+//#define TT_COLOR12     "#2b65ec"
 ///* magenta */
-//#define TINYTERM_COLOR5      "#cd00cd"
-//#define TINYTERM_COLOR13     "#ff00ff"
+//#define TT_COLOR5      "#cd00cd"
+//#define TT_COLOR13     "#ff00ff"
 ///* cyan */
-//#define TINYTERM_COLOR6      "#00cdcd"
-//#define TINYTERM_COLOR14     "#00ffff"
+//#define TT_COLOR6      "#00cdcd"
+//#define TT_COLOR14     "#00ffff"
 ///* white */
-//#define TINYTERM_COLOR7      "#e5e5e5"
-//#define TINYTERM_COLOR15     "#ffffff"
+//#define TT_COLOR7      "#e5e5e5"
+//#define TT_COLOR15     "#ffffff"
 
-/* Keyboard shortcuts */
-#define TINYTERM_MODIFIER       GDK_CONTROL_MASK | GDK_SHIFT_MASK
-#define TINYTERM_KEY_COPY       GDK_C
-#define TINYTERM_KEY_PASTE      GDK_V
-#define TINYTERM_KEY_OPEN       GDK_O   // pass selected text to xdg-open
-#define TINYTERM_KEY_URL_INIT   GDK_U
-#define TINYTERM_KEY_URL_NEXT   GDK_J   // only in url select mode, not matched against modifier
-#define TINYTERM_KEY_URL_PREV   GDK_K   // only in url select mode, not matched against modifier
+/* shortcuts */
+#define TT_MODIFIER       GDK_CONTROL_MASK | GDK_SHIFT_MASK
+#define TT_KEY_COPY       GDK_C
+#define TT_KEY_PASTE      GDK_V
+#define TT_KEY_OPEN       GDK_O   // pass selected text to xdg-open
+#define TT_KEY_URL_INIT   GDK_U
+#define TT_KEY_URL_NEXT   GDK_J   // only in url select mode, not matched against modifier
+#define TT_KEY_URL_PREV   GDK_K   // only in url select mode, not matched against modifier
 
-/* Regular expression matching urls */
+/* regex matching urls */
 #define SPECIAL_CHARS   "[[:alnum:]\\Q+-_,?;.:/!%$^*&~#=()\\E]"
 #define SCHEME          "(?:[[:alpha:]][+-.[:alpha:]]*://)"
 #define USERINFO        "(?:[[:alnum:]]+(?:" SPECIAL_CHARS "+)?\\@)?"
--- bin/c/tinyterm/tt.c
+++ bin/c/tinyterm/tt.c
@@ -1,4 +1,4 @@
-// Ryan P.C. McQuen | Everett, WA | ryan.q@linux.com
+/* Ryan P.C. McQuen | Everett, WA | ryan.q@linux.com */
 
 #include <stdlib.h>
 #include <glib.h>
@@ -11,7 +11,7 @@
 static gboolean url_select_mode = FALSE;
 static int child_pid = 0;   // needs to be global for signal_handler to work
 
-/* spawn xdg-open and pass text as argument */
+/* xdg-open, text is argument */
 static void
 xdg_open(const char* text)
 {
@@ -25,14 +25,14 @@ xdg_open(const char* text)
     g_free(command);
 }
 
-/* callback to receive data from GtkClipboard */
+/* cb, get data from gtkclipboard */
 static void
 xdg_open_selection_cb(GtkClipboard* clipboard, const char* string, gpointer data)
 {
     xdg_open(string);
 }
 
-/* pass selected text to xdg-open */
+/* selected text => xdg-open */
 static void
 xdg_open_selection(GtkWidget* terminal)
 {
@@ -42,14 +42,14 @@ xdg_open_selection(GtkWidget* terminal)
     gtk_clipboard_request_text(clipboard, xdg_open_selection_cb, NULL);
 }
 
-/* callback to set window urgency hint on beep events */
+/* cb, window urgency hint @ beep */
 static void
 window_urgency_hint_cb(VteTerminal* vte)
 {
     gtk_window_set_urgency_hint(GTK_WINDOW (gtk_widget_get_toplevel(GTK_WIDGET (vte))), TRUE);
 }
 
-/* callback to unset window urgency hint on focus */
+/* unset on focus */
 gboolean
 window_focus_cb(GtkWindow* window)
 {
@@ -57,23 +57,23 @@ window_focus_cb(GtkWindow* window)
     return FALSE;
 }
 
-/* callback to dynamically change window title */
+/* dynamic window title */
 static void
 window_title_cb(VteTerminal* vte)
 {
     gtk_window_set_title(GTK_WINDOW (gtk_widget_get_toplevel(GTK_WIDGET (vte))), vte_terminal_get_window_title(vte));
 }
 
-/* callback to react to key press events */
+/* respond @ key presses */
 static gboolean
 key_press_cb(VteTerminal* vte, GdkEventKey* event)
 {
     if (url_select_mode) {
         switch (gdk_keyval_to_upper(event->keyval)) {
-            case TINYTERM_KEY_URL_NEXT:
+            case TT_KEY_URL_NEXT:
                 vte_terminal_search_find_next(vte);
                 return TRUE;
-            case TINYTERM_KEY_URL_PREV:
+            case TT_KEY_URL_PREV:
                 vte_terminal_search_find_previous(vte);
                 return TRUE;
             case GDK_Return:
@@ -85,18 +85,18 @@ key_press_cb(VteTerminal* vte, GdkEventKey* event)
         }
         return TRUE;
     }
-    if ((event->state & (TINYTERM_MODIFIER)) == (TINYTERM_MODIFIER)) {
+    if ((event->state & (TT_MODIFIER)) == (TT_MODIFIER)) {
         switch (gdk_keyval_to_upper(event->keyval)) {
-            case TINYTERM_KEY_COPY:
+            case TT_KEY_COPY:
                 vte_terminal_copy_clipboard(vte);
                 return TRUE;
-            case TINYTERM_KEY_PASTE:
+            case TT_KEY_PASTE:
                 vte_terminal_paste_clipboard(vte);
                 return TRUE;
-            case TINYTERM_KEY_OPEN:
+            case TT_KEY_OPEN:
                 xdg_open_selection(vte);
                 return TRUE;
-            case TINYTERM_KEY_URL_INIT:
+            case TT_KEY_URL_INIT:
                 url_select_mode = vte_terminal_search_find_previous(vte);
                 return TRUE;
         }
@@ -104,7 +104,7 @@ key_press_cb(VteTerminal* vte, GdkEventKey* event)
     return FALSE;
 }
 
-/* callback to block mouse when in url-select mode */
+/* block mouse in url-select mode */
 static gboolean
 button_press_cb(VteTerminal* vte, GdkEventButton* event)
 {
@@ -121,34 +121,34 @@ vte_config(VteTerminal* vte)
     GRegex* regex = g_regex_new(url_regex, G_REGEX_CASELESS, G_REGEX_MATCH_NOTEMPTY, NULL);
 
     vte_terminal_search_set_gregex(vte, regex);
-    vte_terminal_search_set_wrap_around     (vte, TINYTERM_SEARCH_WRAP_AROUND);
-    vte_terminal_set_audible_bell           (vte, TINYTERM_AUDIBLE_BELL);
-    vte_terminal_set_visible_bell           (vte, TINYTERM_VISIBLE_BELL);
-    vte_terminal_set_cursor_shape           (vte, TINYTERM_CURSOR_SHAPE);
-    vte_terminal_set_cursor_blink_mode      (vte, TINYTERM_CURSOR_BLINK);
-    vte_terminal_set_word_chars             (vte, TINYTERM_WORD_CHARS);
-    vte_terminal_set_scrollback_lines       (vte, TINYTERM_SCROLLBACK_LINES);
-    vte_terminal_set_font_from_string_full  (vte, TINYTERM_FONT, TINYTERM_ANTIALIAS);
+    vte_terminal_search_set_wrap_around     (vte, TT_SEARCH_WRAP_AROUND);
+    vte_terminal_set_audible_bell           (vte, TT_AUDIBLE_BELL);
+    vte_terminal_set_visible_bell           (vte, TT_VISIBLE_BELL);
+    vte_terminal_set_cursor_shape           (vte, TT_CURSOR_SHAPE);
+    vte_terminal_set_cursor_blink_mode      (vte, TT_CURSOR_BLINK);
+    vte_terminal_set_word_chars             (vte, TT_WORD_CHARS);
+    vte_terminal_set_scrollback_lines       (vte, TT_SCROLLBACK_LINES);
+    vte_terminal_set_font_from_string_full  (vte, TT_FONT, TT_ANTIALIAS);
 
-    /* init colors */
-    gdk_color_parse(TINYTERM_COLOR_FOREGROUND, &color_fg);
-    gdk_color_parse(TINYTERM_COLOR_BACKGROUND, &color_bg);
-    gdk_color_parse(TINYTERM_COLOR0,  &color_palette[0]);
-    gdk_color_parse(TINYTERM_COLOR1,  &color_palette[1]);
-    gdk_color_parse(TINYTERM_COLOR2,  &color_palette[2]);
-    gdk_color_parse(TINYTERM_COLOR3,  &color_palette[3]);
-    gdk_color_parse(TINYTERM_COLOR4,  &color_palette[4]);
-    gdk_color_parse(TINYTERM_COLOR5,  &color_palette[5]);
-    gdk_color_parse(TINYTERM_COLOR6,  &color_palette[6]);
-    gdk_color_parse(TINYTERM_COLOR7,  &color_palette[7]);
-    gdk_color_parse(TINYTERM_COLOR8,  &color_palette[8]);
-    gdk_color_parse(TINYTERM_COLOR9,  &color_palette[9]);
-    gdk_color_parse(TINYTERM_COLOR10, &color_palette[10]);
-    gdk_color_parse(TINYTERM_COLOR11, &color_palette[11]);
-    gdk_color_parse(TINYTERM_COLOR12, &color_palette[12]);
-    gdk_color_parse(TINYTERM_COLOR13, &color_palette[13]);
-    gdk_color_parse(TINYTERM_COLOR14, &color_palette[14]);
-    gdk_color_parse(TINYTERM_COLOR15, &color_palette[15]);
+    /* init colours */
+    gdk_color_parse(TT_COLOR_FOREGROUND, &color_fg);
+    gdk_color_parse(TT_COLOR_BACKGROUND, &color_bg);
+    gdk_color_parse(TT_COLOR0,  &color_palette[0]);
+    gdk_color_parse(TT_COLOR1,  &color_palette[1]);
+    gdk_color_parse(TT_COLOR2,  &color_palette[2]);
+    gdk_color_parse(TT_COLOR3,  &color_palette[3]);
+    gdk_color_parse(TT_COLOR4,  &color_palette[4]);
+    gdk_color_parse(TT_COLOR5,  &color_palette[5]);
+    gdk_color_parse(TT_COLOR6,  &color_palette[6]);
+    gdk_color_parse(TT_COLOR7,  &color_palette[7]);
+    gdk_color_parse(TT_COLOR8,  &color_palette[8]);
+    gdk_color_parse(TT_COLOR9,  &color_palette[9]);
+    gdk_color_parse(TT_COLOR10, &color_palette[10]);
+    gdk_color_parse(TT_COLOR11, &color_palette[11]);
+    gdk_color_parse(TT_COLOR12, &color_palette[12]);
+    gdk_color_parse(TT_COLOR13, &color_palette[13]);
+    gdk_color_parse(TT_COLOR14, &color_palette[14]);
+    gdk_color_parse(TT_COLOR15, &color_palette[15]);
 
     vte_terminal_set_colors(vte, &color_fg, &color_bg, &color_palette, 16);
 }
@@ -159,7 +159,7 @@ vte_spawn(VteTerminal* vte, char* working_directory, char* command, char** envir
     GError* error = NULL;
     char** command_argv = NULL;
 
-    /* Parse command into array */
+    /* command => array */
     if (!command)
         command = vte_get_user_shell();
     g_shell_parse_argv(command, NULL, &command_argv, &error);
@@ -169,17 +169,17 @@ vte_spawn(VteTerminal* vte, char* working_directory, char* command, char** envir
         exit(EXIT_FAILURE);
     }
 
-    /* Create pty object */
+    /* create pty object */
     VtePty* pty = vte_terminal_pty_new(vte, VTE_PTY_NO_HELPER, &error);
     if (error) {
         g_printerr("Failed to create pty: %s\n", error->message);
         g_error_free(error);
         exit(EXIT_FAILURE);
     }
-    vte_pty_set_term(pty, TINYTERM_TERMINFO);
+    vte_pty_set_term(pty, TT_TERMINFO);
     vte_terminal_set_pty_object(vte, pty);
 
-    /* Spawn default shell (or specified command) */
+    /* default shell (or command) */
     g_spawn_async(working_directory, command_argv, environment,
                   (G_SPAWN_DO_NOT_REAP_CHILD | G_SPAWN_SEARCH_PATH | G_SPAWN_LEAVE_DESCRIPTORS_OPEN),  // flags from GSpawnFlags
                   (GSpawnChildSetupFunc)vte_pty_child_setup, // an extra child setup function to run in the child just before exec()
@@ -195,7 +195,7 @@ vte_spawn(VteTerminal* vte, char* working_directory, char* command, char** envir
     g_strfreev(command_argv);
 }
 
-/* callback to exit TinyTerm with exit status of child process */
+/* exit tt; exit status of child process */
 static void
 vte_exit_cb(VteTerminal* vte)
 {
@@ -207,14 +207,14 @@ vte_exit_cb(VteTerminal* vte)
 static void
 parse_arguments(int argc, char* argv[], char** command, char** directory, gboolean* keep, char** name, char** title)
 {
-    gboolean version = FALSE;   // show version?
+    gboolean version = FALSE;   // version(?)
     const GOptionEntry entries[] = {
-        {"version",   'v', 0, G_OPTION_ARG_NONE,    &version,   "Display program version and exit.", 0},
-        {"execute",   'e', 0, G_OPTION_ARG_STRING,  command,    "Execute command instead of default shell.", "COMMAND"},
-        {"directory", 'd', 0, G_OPTION_ARG_STRING,  directory,  "Sets the working directory for the shell (or the command specified via -e).", "PATH"},
-        {"keep",      'k', 0, G_OPTION_ARG_NONE,    keep,       "Don't exit the terminal after child process exits.", 0},
-        {"name",      'n', 0, G_OPTION_ARG_STRING,  name,       "Set first value of WM_CLASS property; second value is always 'TinyTerm' (default: 'tinyterm')", "NAME"},
-        {"title",     't', 0, G_OPTION_ARG_STRING,  title,      "Set value of WM_NAME property; disables window_title_cb (default: 'TinyTerm')", "TITLE"},
+        {"version",   'v', 0, G_OPTION_ARG_NONE,    &version,   "show version; exit", 0},
+        {"execute",   'e', 0, G_OPTION_ARG_STRING,  command,    "execute command rather than shell", "COMMAND"},
+        {"directory", 'd', 0, G_OPTION_ARG_STRING,  directory,  "set working dir of shell (or command given with -e)", "PATH"},
+        {"keep",      'k', 0, G_OPTION_ARG_NONE,    keep,       "keep tt open after child process exits", 0},
+        {"name",      'n', 0, G_OPTION_ARG_STRING,  name,       "set first value of WM_CLASS; second is 'tt'", "NAME"},
+        {"title",     't', 0, G_OPTION_ARG_STRING,  title,      "set value of WM_NAME; disables window_title_cb", "TITLE"},
         { NULL }
     };
 
@@ -232,12 +232,12 @@ parse_arguments(int argc, char* argv[], char** command, char** directory, gboole
     }
 
     if (version) {
-        g_print("tinyterm " TINYTERM_VERSION "\n");
+        g_print("tt " TT_VERSION "\n");
         exit(EXIT_SUCCESS);
     }
 }
 
-/* UNIX signal handler */
+/* signal handler */
 static void
 signal_handler(int signal)
 {
@@ -256,7 +256,7 @@ main (int argc, char* argv[])
     GtkIconTheme* icon_theme;
     GError* error = NULL;
 
-    /* Variables for parsed command-line arguments */
+    /* command-line arguments parser vars */
     char* command = NULL;
     char* directory = NULL;
     gboolean keep = FALSE;
@@ -266,16 +266,16 @@ main (int argc, char* argv[])
     gtk_init(&argc, &argv);
     parse_arguments(argc, argv, &command, &directory, &keep, &name, &title);
 
-    /* Create window */
+    /* window */
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     g_signal_connect(window, "delete-event", gtk_main_quit, NULL);
-    gtk_window_set_wmclass(GTK_WINDOW (window), name ? name : "tinyterm", "TinyTerm");
-    gtk_window_set_title(GTK_WINDOW (window), title ? title : "TinyTerm");
+    gtk_window_set_wmclass(GTK_WINDOW (window), name ? name : "tt", "tt");
+    gtk_window_set_title(GTK_WINDOW (window), title ? title : "tt");
 
-    /* set default window size, defined in config.h */
-    gtk_window_set_default_size(GTK_WINDOW (window), TINYTERM_X_WINDOW_SIZE, TINYTERM_Y_WINDOW_SIZE);
+    /* window size (config.h) */
+    gtk_window_set_default_size(GTK_WINDOW (window), TT_X_WINDOW_SIZE, TT_Y_WINDOW_SIZE);
 
-    /* Set window icon supplied by an icon theme */
+    /* window icon (from theme) */
     icon_theme = gtk_icon_theme_get_default();
     icon = gtk_icon_theme_load_icon(icon_theme, "terminal", 48, 0, &error);
     if (error)
@@ -283,31 +283,31 @@ main (int argc, char* argv[])
     if (icon)
         gtk_window_set_icon(GTK_WINDOW (window), icon);
 
-    /* Create main box */
+    /* box */
     box = gtk_hbox_new(FALSE, 0);
     gtk_container_add(GTK_CONTAINER (window), box);
 
-    /* Create vte terminal widget */
+    /* vte widget */
     GtkWidget* vte_widget = vte_terminal_new();
     gtk_box_pack_start(GTK_BOX (box), vte_widget, TRUE, TRUE, 0);
     VteTerminal* vte = VTE_TERMINAL (vte_widget);
     if (!keep)
         g_signal_connect(vte, "child-exited", G_CALLBACK (vte_exit_cb), NULL);
     g_signal_connect(vte, "key-press-event", G_CALLBACK (key_press_cb), NULL);
-    #ifdef TINYTERM_URGENT_ON_BELL
+    #ifdef TT_URGENT_ON_BELL
     g_signal_connect(vte, "beep", G_CALLBACK (window_urgency_hint_cb), NULL);
     g_signal_connect(window, "focus-in-event",  G_CALLBACK (window_focus_cb), NULL);
     g_signal_connect(window, "focus-out-event", G_CALLBACK (window_focus_cb), NULL);
-    #endif // TINYTERM_URGENT_ON_BELL
-    #ifdef TINYTERM_URL_BLOCK_MOUSE
+    #endif // TT_URGENT_ON_BELL
+    #ifdef TT_URL_BLOCK_MOUSE
     g_signal_connect(vte, "button-press-event", G_CALLBACK (button_press_cb), NULL);
-    #endif // TINYTERM_URL_BLOCK_MOUSE
-    #ifdef TINYTERM_DYNAMIC_WINDOW_TITLE
+    #endif // TT_URL_BLOCK_MOUSE
+    #ifdef TT_DYNAMIC_WINDOW_TITLE
     if (!title)
         g_signal_connect(vte, "window-title-changed", G_CALLBACK (window_title_cb), NULL);
-    #endif // TINYTERM_DYNAMIC_WINDOW_TITLE
+    #endif // TT_DYNAMIC_WINDOW_TITLE
 
-    /* Apply geometry hints to handle terminal resizing */
+    /* geometry hints (window resizing) */
     geo_hints.base_width  = vte->char_width;
     geo_hints.base_height = vte->char_height;
     geo_hints.min_width   = vte->char_width;
@@ -317,17 +317,17 @@ main (int argc, char* argv[])
     gtk_window_set_geometry_hints(GTK_WINDOW (window), vte_widget, &geo_hints,
                                   GDK_HINT_RESIZE_INC | GDK_HINT_MIN_SIZE | GDK_HINT_BASE_SIZE);
 
-    /* Create scrollbar */
-    #ifdef TINYTERM_SCROLLBAR_VISIBLE
+    /* scrollbar */
+    #ifdef TT_SCROLLBAR_VISIBLE
     GtkWidget* scrollbar;
     scrollbar = gtk_vscrollbar_new(vte->adjustment);
     gtk_box_pack_start(GTK_BOX (box), scrollbar, FALSE, FALSE, 0);
-    #endif // TINYTERM_SCROLLBAR_VISIBLE
+    #endif // TT_SCROLLBAR_VISIBLE
 
     vte_config(vte);
     vte_spawn(vte, directory, command, NULL);
 
-    /* register signal handler */
+    /* signal handler */
     signal(SIGHUP, signal_handler);
     signal(SIGINT, signal_handler);
     signal(SIGTERM, signal_handler);
@@ -338,7 +338,7 @@ main (int argc, char* argv[])
     g_free(name);
     g_free(title);
 
-    /* Show widgets and run main loop */
+    /* show; run main */
     gtk_widget_show_all(window);
     gtk_main();
 
