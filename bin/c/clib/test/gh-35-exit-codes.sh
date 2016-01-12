8c31d0be7ff4cb2c5556feb19c1ccef5801cab2c
zacanger <zac@zacanger.com>
2016-01-12 08:37:56 -0700

    foo

--- bin/c/clib/test/gh-35-exit-codes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# see #35
-
-clib install stephenmathieson/rot132.c > /dev/null 2>&1
-[ $? -ne 0 ] || {
-  echo >&2 "Failed installations should exit with a non-zero exit code";
-  exit 1
-}
