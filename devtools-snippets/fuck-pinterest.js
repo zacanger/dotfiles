b2c9e873c12524ce7e431e8c1175ebf293ffe333
zacanger <zac@zacanger.com>
2016-02-05 11:51:41 -0700

    foo

--- devtools-snippets/fuck-pinterest.js
+++ devtools-snippets/fuck-pinterest.js
@@ -1,4 +1,5 @@
-document.getElementByClassName('quarterScreenBanner')[0].remove()
-document.getElementByClassName('InlineSignup')[0].remove()
-document.getElementByClassName('UnauthBanner')[0].remove()
-document.getElementByClassName('Nags')[0].remove()
+document.getElementsByClassName('quarterScreenBanner')[0].remove()
+document.getElementsByClassName('InlineSignup')[0].remove()
+document.getElementsByClassName('UnauthBanner')[0].remove()
+document.getElementsByClassName('Nags')[0].remove()
+
