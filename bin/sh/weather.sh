f6301b9c8e793ca9fbc90fd3f6565765089a67ac
zacanger <zac@zacanger.com>
2016-02-14 19:43:58 -0700

    adding '.sh' to every damned shell script

--- bin/sh/weather.sh
+++ bin/sh/weather.sh
@@ -1,30 +1,109 @@
 #!/bin/sh
-SEDSTR='
-        # grab the three lines we are interested in
-        127,130!d;
+#
+# Show current weather and temperature via OpenWeatherMap [1]. The weather
+# icon requires a special font [2].
+#
+# [1] http://openweathermap.org/
+# [2] http://erikflowers.github.io/weather-icons/
 
-        # add parens around the first line
-        s/\([^\n]*\)/(\1)/;
+API="http://api.openweathermap.org/data/2.5/forecast?q=%s&units=imperial&mode=xml&appid=be22f778633d53500a0fe39dc09ba5d5"
 
-        # join the first two lines
-        N;
-        N;
+today() {
+    curl -s --connect-timeout 5 $(printf $API $@)
+}
 
-        # add two slashes at the end
-        s/\(.*\)/\1 \/\//;
+temperature() {
+    xmllint --xpath 'string(//current/temperature/@value)' -
+}
 
-        # join the last line
-        N;
+icon() {
+    xmllint --xpath 'string(//current/weather/@icon)' -
+}
 
-        # remove all html
-        s/<[^>]*>//g;
 
-        # remove any newlines
-        s/\n/ /g;
+main() {
+    xml=$(today $1)
 
-        # replace html escape with actual char
-        s/&deg;/°/;'
+    case $(echo $xml | icon) in
+        #== day ==#
+        #
+        # clear sky
+        01d)
+            printf " "
+            ;;
+        # few-clouds
+        02d)
+            printf " "
+            ;;
+        # scattered-clouds
+        03d)
+            printf " "
+            ;;
+        # broken-clouds
+        04d)
+            printf " "
+            ;;
+        # shower rain
+        09d)
+            printf " "
+            ;;
+        # rain
+        10d)
+            printf " "
+            ;;
+        # thunderstorm
+        11d)
+            printf " "
+            ;;
+        # snow
+        13d)
+            printf " "
+            ;;
+        # mist
+        50d)
+            printf " "
+            ;;
+        #== night ==#
+        #
+        # clear sky
+        01n)
+            printf " "
+            ;;
+        # few-clouds
+        02n)
+            printf " "
+            ;;
+        # scattered-clouds
+        03n)
+            printf " "
+            ;;
+        # broken-clouds
+        04n)
+            printf " "
+            ;;
+        # shower rain
+        09n)
+            printf " "
+            ;;
+        # rain
+        10n)
+            printf " "
+            ;;
+        # thunderstorm
+        11n)
+            printf " "
+            ;;
+        # snow
+        13n)
+            printf " "
+            ;;
+        # mist
+        50n)
+            printf " "
+            ;;
+    esac
 
-[ -z $1 ] && zip=84601 || zip=$1
-curl "http://thefuckingweather.com/?zipcode=$zip" -s  | sed "$SEDSTR"
+    printf "%0.1f °F\n" $(echo $xml | temperature)
+}
 
+main $@
