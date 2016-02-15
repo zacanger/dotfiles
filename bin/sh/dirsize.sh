95bff7c8b514b3d2e3532c29a90be556b9304290
zacanger <zac@zacanger.com>
2016-02-14 21:53:51 -0700

    foo

--- bin/sh/dirsize.sh
+++ bin/sh/dirsize.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-. ~/bin/sh/shell-colors
+. ~/bin/sh/shell-colors.sh
 [ "$1" = "" ] && printf "${yell}Usage : $0 /path/to/dir \n ${reset}" && exit
 printf ${green}
 du -hs --apparent-size $1
 printf ${reset}
 
 exit 0
+
