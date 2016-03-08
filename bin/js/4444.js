e4e05fae5c870b0ba2b6cb310d6c8f6d90ff736b
zacanger <zac@zacanger.com>
2016-03-07 18:59:37 -0700

    :arrow_double_up: 4444.js

--- bin/js/4444.js
+++ bin/js/4444.js
@@ -7,22 +7,22 @@
 // the right server for you.
 
 var http  = require('http')
-	, fs    = require('fs')
-	, path  = require('path')
-	, index = path.resolve(__dirname, './index.html')
-	, port  = 4444
+  , fs    = require('fs')
+  , path  = require('path')
+  , index = path.resolve(__dirname, './index.html')
+  , port  = 4444
 
 http.createServer(function(req, res){
-	var stream = fs.createReadStream(index)
-	stream.on('open', function(){
-		res.writeHead(200, {'Content-Type': 'text/html'})
-	})
-	stream.on('error', function(){
-		res.writeHead(400)
-		res.end()
-	})
-	stream.pipe(res)
-}).listen(4444)
+  var stream = fs.createReadStream(index)
+  stream.on('open', function(){
+    res.writeHead(200, {'Content-Type': 'text/html'})
+  })
+  stream.on('error', function(){
+    res.writeHead(400)
+    res.end()
+  })
+  stream.pipe(res)
+}).listen(port)
 
 console.log('check over at ' + port)
 
