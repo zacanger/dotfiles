e02f3470fa89a7ad2167e5e33fd1f334a41a3f57
zacanger <zac@zacanger.com>
2016-03-10 00:20:37 -0700

    :clock630: shalarm.sh

--- bin/sh/shalarm.sh
+++ bin/sh/shalarm.sh
@@ -5,13 +5,12 @@ command="$2"
 trig=0
 
 while [ $trig = 0 ]; do
-	if [ "$(date +%R)" = $time  ]; then
-		$command
-		trig=1
-	else
-		sleep 5
-
-	fi
+  if [ "$(date +%R)" = $time  ]; then
+    $command
+    trig=1
+  else
+    sleep 5
+  fi
 done
 exit
 
