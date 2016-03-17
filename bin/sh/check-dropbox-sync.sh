7ac7f04d7ae6dc59428c5045be55a850bdcb80e4
zacanger <zac@zacanger.com>
2016-03-16 22:45:13 -0600

    :checkered_flag: check dropbox sync

---
+++ bin/sh/check-dropbox-sync.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# the story behind this script:
+# it's 16th march, 2016, and i've just finally treated myself to
+# the 1TB dropbox subscription (a HUGE upgrade from the 8 gigs i've
+# been using). obviously i've just thrown hundreds of gigs of stuff
+# into my dropbox. i just need a simple thing to tell me what's going
+# on with the sync every once-in-a-while, so i can stop typing `dbst`
+# (an alias for `dropbox status`) every couple of minutes.
+# so... this script.
+
+while true
+do
+  sleep 300
+  dropbox filestatus
+done
+
