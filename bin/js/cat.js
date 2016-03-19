ecd01054b4380327e4bf3ef2f5f25a5d2f4a6c44
zacanger <zac@zacanger.com>
2016-03-18 23:35:15 -0600

    :negative_squared_cross_mark: cat.js

--- bin/js/cat.js
+++ bin/js/cat.js
@@ -1,4 +1,22 @@
 #!/usr/bin/env node
 
-require('fs').createReadStream(process.argv[2]).pipe(process.stdout)
+'use strict'
 
+// there's another file ('cat-tiny.js') in this repo which just reads the first
+// argument and pipes it to stdout. i like that one because it's one line
+// (two including the shebang). but here's a better attempt at `cat(1)` in node.
+// :)
+
+const
+  fs   = require('fs')
+, args = process.argv.slice(2)
+
+if(!args[0]){
+  console.error('please specify file(s) to catenate')
+}
+
+else {
+  for(let i = 0; i < args.length; i++){
+    fs.createReadStream(args[i]).pipe(process.stdout)
+  }
+}
