b6f21c92500f5dcb102e196cea37215889d84324
zacanger <zac@zacanger.com>
2016-03-20 21:19:18 -0600

    :no_good: quick server thing

--- bin/js/min.js
+++
@@ -1,3 +0,0 @@
-min = function (number1, number2) {
-  return (number1 < number2) ? number1 : number2
-}
---
+++ bin/js/quick-server.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const
+  http   = require('http')
+, path   = require('path')
+, fs     = require('fs')
+, port   = process.env.PORT || 4444
+, server = http.createServer((req, res) => {
+
+  let filePath = '.' + req.url
+  if (filePath == './') {
+    filePath = './index.html'
+  }
+
+  let extname = path.extname(filePath)
+
+  let contentType = 'text/html'
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript'
+      break
+    case '.css':
+      contentType = 'text/css'
+      break
+    case '.json':
+      contentType = 'application/json'
+      break
+    case '.png':
+      contentType = 'image/png'
+      break
+    case '.jpg':
+      contentType = 'image/jpg'
+      break
+    case '.wav':
+      contentType = 'audio/wav'
+      break
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        fs.readFile('./404.html', (err, content) => {
+          res.writeHead(200, {'Content-Type' : contentType})
+          res.end(content, 'utf-8')
+        })
+      } else {
+        res.writeHead(500)
+        res.end('Server error ' + err.code + '\n')
+        res.end()
+      }
+    } else {
+      res.writeHead(200, {'Content-Type' : contentType})
+      res.end(content, 'utf-8')
+    }
+  })
+
+})
+
+server.listen(port, () => {
+  console.log('server listening on', port)
+})
+
