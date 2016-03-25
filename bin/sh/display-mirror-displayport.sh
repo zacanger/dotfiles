52804eda6e3f91165e0f9000cc8692cd99c3c9a2
zacanger <zac@zacanger.com>
2016-03-25 14:51:36 -0600

    :seedling: i had to install vscode for two minutes. thank god that's over.

--- bin/sh/display-mirror-displayport.sh
+++ bin/sh/display-mirror-displayport.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
 # for mirroring my display when on displayport.
-# not sure which one of these is actually the right displayport, yet,
-# since xrandr lists three (and i definitely only have one)....
-
-# xrandr --output DisplayPort-0 --auto --same-as LVDS
-# xrandr --output DisplayPort-1 --auto --same-as LVDS
-# xrandr --output DisplayPort-2 --auto --same-as LVDS
+xrandr --output DisplayPort-0 --auto --same-as LVDS
 
