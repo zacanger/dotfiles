4265af93609e6a62a171fbd0dd2322f6d0961392
zacanger <zac@zacanger.com>
2016-04-18 10:06:47 -0600

    foo

---
+++ bin/sh/do-update-forever-nginx.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "Ready?"
+sleep 2
+
+apt update
+apt full-upgrade -y --allow-unauthenticated --fix-missing
+
+npm i -g n
+n latest
+npm i -g npm
+npm update -g
+forever restartall
+service nginx restart
+
+clear
+echo "Done!"
+
