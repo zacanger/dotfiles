4898a196f672b6b979cc84ff6229d7a9b6185a2a
zacanger <zac@zacanger.com>
2016-05-01 15:40:49 -0600

    foo

--- bin/sh/gif-index-simple.sh
+++ bin/sh/gif-index-simple.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# don't use this if you have a lot of gifs
+# it puts every gif in an image tag with a link
+
 echo "<!doctype html><html lang=\"en\"><body><h1><a href=\"/\">home</a></h1>" > index.html
 ls -1 | grep .gif$ | awk '{ printf "<a href=\"%s\"><img src=\"%s\"></a>\n",$1,$1,$1 }' >> index.html
 echo "</body></html>" >> index.html
