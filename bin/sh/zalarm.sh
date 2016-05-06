22358b344c68b0c23cce74031e537d0853ba92ae
zacanger <zac@zacanger.com>
2016-05-06 10:48:22 -0600

    :alarm_clock: alarm

--- bin/sh/zalarm.sh
+++ bin/sh/zalarm.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-# run like so:
-# zalarm.sh 07:45 mplayer2 ~/.alarm.ogg
+# this is a trial
 
-time="$1"
-command="$2"
-trig=0
+printf "date/time? "
 
-while [ $trig = 0 ]; do
-  if [ "$(date +%R)" = $time  ]; then
-    $command
-    trig=1
-  else
-    sleep 5
-  fi
+read date
+
+echo okeedokee. alarm happening at $(date --date="$date")
+
+sleep $(( $(date --date="$date" +%s) - $(date +%s) ))
+
+echo WAKE THE FUCK UP
+
+while true; do
+  /usr/bin/mpv /home/z/Dropbox/z/.alarm.mp3
+  sleep 1
 done
-exit
 
