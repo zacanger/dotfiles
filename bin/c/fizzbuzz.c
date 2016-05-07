12ee5e39beaa6ac54a47a870cc7b5edf7f1db242
zacanger <zac@zacanger.com>
2016-05-07 14:04:20 -0600

    :low_brightness: vim session

--- bin/c/fizzbuzz.c
+++ bin/c/fizzbuzz.c
@@ -4,17 +4,17 @@
 
 int main(){
   printf("Fizzbuzz from 1 through 100.\n");
-	int hundred;
-	for (hundred = 1; hundred <= 100; hundred++){
-		printf("%d", hundred);
-		if (hundred % 3 == 0) {
-			printf("Fizz");
-		} if (hundred % 5 == 0) {
-			printf("Buzz\n");
-		} else {
-			printf("\n");
-		}
-	}
-	return 0;
+  int hundred;
+  for (hundred = 1; hundred <= 100; hundred++){
+    printf("%d", hundred);
+    if (hundred % 3 == 0) {
+      printf("Fizz");
+    } if (hundred % 5 == 0) {
+      printf("Buzz\n");
+    } else {
+      printf("\n");
+    }
+  }
+  return 0;
 }
 
