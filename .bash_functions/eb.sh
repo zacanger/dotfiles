731bd4fedefa1f196cbdc8715265230b3805163c
zacanger <zac@zacanger.com>
2016-06-16 13:27:35 -0600

    eb

---
+++ .bash_functions/eb.sh
@@ -0,0 +1,23 @@
+# for dealing with aws elasticbeanstalk
+# needs the aws cli and jq
+
+eb_describe(){
+  aws elasticbeanstalk describe-environments --application-name "$APPLICATION_NAME" --environment-names "$ENVIRONMENT_NAME"
+}
+
+environment_status(){
+  echo "$(eb_describe)" | jq -r .Environments[0].Status
+}
+
+environment_health(){
+  echo "$(eb_describe)" | jq -r .Environments[0].Health
+}
+
+environment_version(){
+  echo "$(eb_describe)" | jq -r .Environments[0].VersionLabel
+}
+
+environment_cname(){
+  echo "$(eb_describe)" | jq -r .Environments[0].CNAME
+}
+
