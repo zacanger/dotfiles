c2e05fc44d864cb92906ee94746c45ca9ffeeb14
zacanger <zac@zacanger.com>
2016-07-01 15:24:55 -0600

    just removing a few things

--- .bash_functions/pidof.sh
+++ .bash_functions/pidof.sh
@@ -1,6 +1,8 @@
 # finds pids for the (single) arg passed
+# because os x doesn't have pidof
+# which is fucking stupid
 
-findpids() {
+pidof() {
   ps -ef | grep -i "$1" | grep -v grep | awk '{print $2}'
 }
 
