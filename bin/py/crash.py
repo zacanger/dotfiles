d4baa7bb38e5b00a70c00641f55d0abd7754c57c
zacanger <zac@zacanger.com>
2016-07-24 22:57:37 -0600

    rm more

--- bin/py/crash.py
+++
@@ -1,1 +0,0 @@
-python -c "while 1:locals().setdefault('i',60);import time,random;print(' '*i+'<>'+' '*(80-i-1)+'|');time.sleep(.1);i+=random.randint(-2,2)"
--- bin/sh/names.sh
+++ bin/sh/names.sh
@@ -1,22 +1,24 @@
-#!/bin/bash
-# Convert filenames to lowercase
-# and replace characters recursively
-#####################################
+#!/usr/bin/env bash
 
-if [ -z "$1" ];then echo Give target directory; exit 0;fi
+# convert filenames to lowercase and replace characters recursively
+
+if [ -z "$1" ]; then
+  echo target directory please
+  exit 0
+fi
 
 find "$1" -depth -name '*' | while read file ; do
-        directory=$(dirname "$file")
-        oldfilename=$(basename "$file")
-        newfilename=$(echo "$oldfilename" | tr 'A-Z' 'a-z' | tr ' ' '_' | sed 's/_-_/-/g')
-        if [ "$oldfilename" != "$newfilename" ]; then
-                mv -i "$directory/$oldfilename" "$directory/$newfilename"
-                echo ""$directory/$oldfilename" ---> "$directory/$newfilename""
-                #echo "$directory"
-                #echo "$oldfilename"
-                #echo "$newfilename"
-                #echo
-                #tr 'A-Z' 'a-z' 
-        fi
-        done
+  directory=$(dirname "$file")
+  oldfilename=$(basename "$file")
+  newfilename=$(echo "$oldfilename" | tr 'A-Z' 'a-z' | tr ' ' '_' | sed 's/_-_/-/g')
+  if [ "$oldfilename" != "$newfilename" ]; then
+    mv -i "$directory/$oldfilename" "$directory/$newfilename"
+    echo ""$directory/$oldfilename" ---> "$directory/$newfilename""
+    #echo "$directory"
+    #echo "$oldfilename"
+    #echo "$newfilename"
+    #echo
+    #tr 'A-Z' 'a-z'
+  fi
+done
 exit 0
