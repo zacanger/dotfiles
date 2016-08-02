73b46b72e5abc7c9e1d007a71153d33bdd667477
zacanger <zac@zacanger.com>
2016-08-01 23:19:01 -0600

    check-up.js

--- bin/js/check-up.js
+++ bin/js/check-up.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
 
-const http = require('http')
-const https = require('https')
-const arg  = process.argv[2]
+const
+  http  = require('http')
+, https = require('https')
+, arg   = process.argv[2]
 
 if (!arg) {
   console.log('Usage: check-up.js example.com')
 } else {
-  let host, port = 80, protocol = http
+  let
+    port     = 80
+  , protocol = http
+  , host
+
   if (arg.includes('https://')) {
     host = arg.replace('https://', '')
     port = 443
@@ -18,16 +23,18 @@ if (!arg) {
   } else {
     host = arg
   }
+
   const opts = {
     host
   , port
   , path : '/'
   }
+
   protocol.get(opts, res => {
     if (res.statusCode.toString().charAt(0) !== '5') {
-      console.log(`${host} is up, ${res.statusCode}`)
+      console.log(`${host} is up, status code ${res.statusCode}`)
     } else {
-      console.log(`${host} is down, ${res.statusCode}`)
+      console.log(`${host} is down, status code ${res.statusCode}`)
     }
   }).on('error', err => {
     console.log(`Error: ${err.message}`)
