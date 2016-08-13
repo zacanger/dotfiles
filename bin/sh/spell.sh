0fa960000252faa72262f45da0d7d7cd7ea47b09
zacanger <zac@zacanger.com>
2016-08-13 13:43:36 -0600

    stuff

--- bin/sh/spell.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# check word spelling
-# depends on hunspell
-
-hash hunspell &>/dev/null || { echo "Hunspell is not installed"; exit 1; }
-
-echo $@ | hunspell -a  | sed '1d' | awk -F ': ' '{ print $2 }'
