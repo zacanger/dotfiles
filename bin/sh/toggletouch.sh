a1a4609180237fe1ea1ea6485a92ff09c3e90d8e
zacanger <zac@zacanger.com>
2016-12-03 15:09:07 -0700

    more github css changes dammit

--- bin/sh/toggletouch.sh
+++ bin/sh/toggletouch.sh
@@ -7,16 +7,15 @@
 PAD='SynPS/2 Synaptics TouchPad'
 A=`xinput list-props "$PAD" | sed -n -e 's/.*Device Enabled ([0-9][0-9]*):\t\(.*\)/\1/p' `
 if [ $A -eq 1 ]; then
-xinput set-int-prop "$PAD" "Device Enabled" 8 0
-notify-send -t 5000 \
-'   touchpad
-░░▒▒▓▓██▓▓▒▒░░' \
-'   disabled'
+  xinput set-int-prop "$PAD" "Device Enabled" 8 0
+  notify-send -t 5000 \
+    '   touchpad
+  ░░▒▒▓▓██▓▓▒▒░░' \
+    '   disabled'
 else
-xinput set-int-prop "$PAD" "Device Enabled" 8 1
-notify-send -t 5000 \
-'   touchpad
-░░▒▒▓▓██▓▓▒▒░░' \
-'   enabled'
+  xinput set-int-prop "$PAD" "Device Enabled" 8 1
+  notify-send -t 5000 \
+    '   touchpad
+  ░░▒▒▓▓██▓▓▒▒░░' \
+    '   enabled'
 fi
-
