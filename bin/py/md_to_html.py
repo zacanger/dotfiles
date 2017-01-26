8a629bc56f406944f56e3bee32864d27c7a997ec
zacanger <zac@zacanger.com>
2017-01-26 08:47:55 -0700

    Some cleanup.

--- bin/py/md_to_html.py
+++ bin/py/md_to_html.py
@@ -5,35 +5,30 @@ import sys
 md = markdown.Markdown()
 
 html_header = '''
-<html>
-<head>
-<link href="github.css" rel="stylesheet"></link>
+<!doctype html><html lang="en"><head>
+<title></title>
+<link href="github.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 <div class="markdown-body">
 '''
 
 html_footer = '''
-</div>
-</body>
-</html>
+</div></body></html>
 '''
 
-
 def convert(file_name):
-    # input file (*.md)
-    md_file = codecs.open(file_name,encoding='utf-8',mode="r")
-    code = markdown.markdown(md_file.read(), extensions=['gfm'])
+  # input file (*.md)
+  md_file = codecs.open(file_name,encoding='utf-8',mode="r")
+  code = markdown.markdown(md_file.read(), extensions=['gfm'])
 
-    # output file (input.md.html)
-    html_file = codecs.open(file_name+".html",encoding='utf-8',mode="w")
-    html_file.write(html_header + code + html_footer)
+  # output file (input.md.html)
+  html_file = codecs.open(file_name+".html",encoding='utf-8',mode="w")
+  html_file.write(html_header + code + html_footer)
 
 if __name__ == "__main__":
-    args = sys.argv
-    if len(args) == 1:
-        print("specify *.md file name from command line.")
-    else:
-        convert(args[1])
-
-
+  args = sys.argv
+  if len(args) == 1:
+    print("specify *.md file name from command line.")
+  else:
+    convert(args[1])
