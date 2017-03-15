8bda2ae1d7ad8638ac07af83676c9c309b908df2
zacanger <zac@zacanger.com>
2017-03-15 09:05:22 -0600

    rm more

--- bin/sh/pipes.sh
+++ bin/sh/pipes.sh
@@ -1,123 +1,60 @@
 #!/usr/bin/env bash
-# pipes.sh: Animated pipes terminal screensaver.
-#
-# This modified version is maintained at:
-#
-#   https://github.com/livibetter/pipes.sh
 
-# options:
-# s = straight [5-15]
-# f = rate [20-100]
-# t = style [0-5]
-# r = ttl
+declare -i f=75 s=13 r=2000 t=0 c=1 n=0 l=0
+declare -ir w=$(tput cols) h=$(tput lines)
+declare -i x=$((w/2)) y=$((h/2))
 
-VERSION=0.1.0
+declare -ar v=( [00]="\x82" [01]="\x8c" [03]="\x90"	####
+	[10]="\x98" [11]="\x80" [12]="\x90"	#  Normal UTF-8 box characters.
+	[21]="\x94" [22]="\x82" [23]="\x98"	#  (These are the default for they are supported almost everywhere.)
+	[30]="\x94" [32]="\x8c" [33]="\x80" )  ####
 
-M=32768
-p=1
-f=75 s=13 r=2000 t=0
-w=$(tput cols) h=$(tput lines)
-# ab -> idx = a*4 + b
-# 0: up, 1: right, 2: down, 3: left
-# 00 means going up   , then going up   -> ┃
-# 12 means going right, then going down -> ┓
-sets=(
-    "┃┏ ┓┛━┓  ┗┃┛┗ ┏━"
-    "│╭ ╮╯─╮  ╰│╯╰ ╭─"
-    "│┌ ┐┘─┐  └│┘└ ┌─"
-    "║╔ ╗╝═╗  ╚║╝╚ ╔═"
-    "|+ ++-+  +|++ +-"
-    "|/ \/-\  \|/\ /-"
-)
-v=()
-RNDSTART=0
-NOCOLOR=0
+# declare -ar v=( [00]="\x83" [01]="\x8f" [03]="\x93"	####
+#	 [10]="\x9b" [11]="\x81" [12]="\x93"	#  Heavy UTF-8 box characters.
+#	 [21]="\x97" [22]="\x83" [23]="\x9b"	#  (Uncomment only if your terminal supports it)
+#	 [30]="\x97" [32]="\x8f" [33]="\x81" )  ####
 
 OPTIND=1
-while getopts "p:t:f:s:r:RChv" arg; do
-case $arg in
-    p) ((p=(OPTARG>0)?OPTARG:p));;
-    t) ((OPTARG>=0 && OPTARG<${#sets[@]})) && V+=($OPTARG);;
-    f) ((f=(OPTARG>19 && OPTARG<101)?OPTARG:f));;
-    s) ((s=(OPTARG>4 && OPTARG<16 )?OPTARG:s));;
-    r) ((r=(OPTARG>=0)?OPTARG:r));;
-    R) RNDSTART=1;;
-    C) NOCOLOR=1;;
-    h) echo -e "Usage: $(basename $0) [OPTION]..."
-        echo -e "Animated pipes terminal screensaver.\n"
-        echo -e " -p [1-]\tnumber of pipes (D=1)."
-        echo -e " -t [0-$((${#sets[@]} - 1))]\ttype of pipes, can be used more than once (D=0)."
-        echo -e " -f [20-100]\tframerate (D=75)."
-        echo -e " -s [5-15]\tprobability of a straight fitting (D=13)."
-        echo -e " -r LIMIT\treset after x characters, 0 if no limit (D=2000)."
-        echo -e " -R \t\trandom starting point."
-        echo -e " -C \t\tno color."
-        echo -e " -h\t\thelp (this screen)."
-        echo -e " -v\t\tprint version number.\n"
-        exit 0;;
-    v) echo "$(basename -- "$0") $VERSION"
-        exit 0
-    esac
-done
+while getopts "f:s:r:h" arg; do
+	case $arg in
+		f) ((f=($OPTARG>19 && $OPTARG<101)?$OPTARG:$f));;
+		s) ((s=($OPTARG>4 && $OPTARG<16 )?$OPTARG:$s));;
+		r) ((r=($OPTARG>0)?$OPTARG:$r));;
+		h) echo -e "Usage: pipes [OPTION]..."
+			echo -e "Animated pipes terminal screensaver.\n"
+			echo -e " -f [20-100]\tframerate (D=75)."
+			echo -e " -s [5-15]\tprobability of a straight fitting (D=13)."
+			echo -e " -r LIMIT\treset after x characters (D=2000)."
+			echo -e " -h\t\thelp (this screen).\n"
+			exit 0;;
+		esac
+	done
 
-# set default values if not by options
-((${#V[@]})) || V=(0)
-
-# Attempt to workaround for Bash versions < 4, such as 3.2 on Mac:
-#   https://gist.github.com/livibetter/4689307/#comment-892368
-# Untested--in conduction of using shebang `env bash`--should fall back to
-# `sleep`
-printf -v SLEEP "read -t0.0$((1000/f)) -n 1"
-if $SLEEP &>/dev/null; (($? != 142)); then
-  printf -v SLEEP "sleep 0.0$((1000/f))"
-fi
-
-cleanup() {
-    # clear up standard input
-    read -t 0 && cat </dev/stdin>/dev/null
-
-    tput rmcup
-    tput cnorm
-    stty echo
-    exit 0
-}
-trap cleanup SIGHUP SIGINT SIGTERM
-
-for (( i=1; i<=p; i++ )); do
-    c[i]=$((i%8)) n[i]=0 l[i]=0
-    ((x[i]=RNDSTART==1?RANDOM*w/32768:w/2))
-    ((y[i]=RNDSTART==1?RANDOM*h/32768:h/2))
-    v[i]=${V[${#V[@]} * RANDOM / M]}
-done
-
-stty -echo
 tput smcup
 tput reset
 tput civis
-# any key press exits the loop and this script
-while $SLEEP; (($? > 128)) || [[ $SLEEP = sleep* ]] && (($? == 0)); do
-    for (( i=1; i<=p; i++ )); do
-        # New position:
-        ((${l[i]}%2)) && ((x[i]+=-${l[i]}+2,1)) || ((y[i]+=${l[i]}-1))
 
-        # Loop on edges (change color on loop):
-        ((${x[i]}>w||${x[i]}<0||${y[i]}>h||${y[i]}<0)) && ((c[i]=RANDOM%8, v[i]=V[${#V[@]}*RANDOM/M]))
-        ((x[i]=(x[i]+w)%w))
-        ((y[i]=(y[i]+h)%h))
+trap "tput rmcup; tput reset; exit" SIGINT
 
-        # New random direction:
-        ((n[i]=RANDOM%s-1))
-        ((n[i]=(${n[i]}>1||${n[i]}==0)?${l[i]}:${l[i]}+${n[i]}))
-        ((n[i]=(${n[i]}<0)?3:${n[i]}%4))
+while true; do
+	# New position:
+	(($l%2)) && ((x+=($l==1)?1:-1))
+	((!($l%2))) && ((y+=($l==2)?1:-1))
 
-        # Print:
-        tput cup ${y[i]} ${x[i]}
-        [[ $NOCOLOR == 0 ]] && echo -ne "\033[1;3${c[i]}m"
-        echo -n "${sets[v[i]]:l[i]*4+n[i]:1}"
-        l[i]=${n[i]}
-    done
-    ((r>0 && t*p>=r)) && tput reset && tput civis && t=0 || ((t++))
+	# Loop on edges (change color on loop):
+	((c=($x>$w || $x<0 || $y>$h || $y<0)?($RANDOM%7):$c))
+	((x=($x>$w)?0:(($x<0)?$w:$x)))
+	((y=($y>$h)?0:(($y<0)?$h:$y)))
+
+	# New random direction:
+	((n=$RANDOM%$s-1))
+	((n=($n>1||$n==0)?$l:$l+$n))
+	((n=($n<0)?3:$n%4))
+
+	# Print:
+	tput cup $y $x
+	printf "\033[1;3${c}m\xe2\x94${v[$l$n]}"
+	(($t>$r)) && tput reset && tput civis && t=0 || ((t++))
+	l=$n
+	sleep $(echo "scale=5;1/$f"|bc)
 done
-
-cleanup
-
