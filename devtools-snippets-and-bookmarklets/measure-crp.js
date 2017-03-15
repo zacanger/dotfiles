6561cbe16c82b0308c944bd3620cedf2f2e6c394
zacanger <zac@zacanger.com>
2017-03-15 13:21:32 -0600

    measure-crp.js

--- devtools-snippets-and-bookmarklets/measure-crp.js
+++
@@ -1,8 +0,0 @@
-const measureCRP = () => {
-  const t = window.performance.timing
-  const interactive = t.domInteractive - t.domLoading
-  const dcl = t.domContentLoadedEventStart - t.domLoading
-  const complete = t.domComplete - t.domLoading
-  console.log({ interactive, dcl, complete })
-}
-document.body.onload = measureCRP()
