25618f8c0004fca6d0847fc1a73172da79c32917
zacanger <zac@zacanger.com>
2017-03-25 16:32:26 -0600

    rm more

--- bin/sh/jsless.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "x$1" == "x-" ]; then
-    jp.py | less
-fi
-
-cat "$1" | jp.py | less
--- bin/sh/leftovers.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-for i in {/usr,~/.local}/share/applications/*.desktop; do which $(grep -Poh '(?<=Exec=).*?( |$)' $i) > /dev/null || echo $i; done
