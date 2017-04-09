fee1188e5c140ca432ee88ab7814a7b0f435a089
zacanger <zac@zacanger.com>
2017-04-08 18:34:49 -0600

    wevver

--- bin/sh/weather.sh
+++ bin/sh/weather.sh
@@ -1,109 +1,15 @@
-#!/bin/sh
-#
-# Show current weather and temperature via OpenWeatherMap [1]. The weather
-# icon requires a special font [2].
-#
-# [1] http://openweathermap.org/
-# [2] http://erikflowers.github.io/weather-icons/
+#!/usr/bin/env bash
 
-API="http://api.openweathermap.org/data/2.5/forecast?q=%s&units=imperial&mode=xml&appid=be22f778633d53500a0fe39dc09ba5d5"
+# usage: weather.sh zip
 
-today() {
-    curl -s --connect-timeout 5 $(printf $API $@)
-}
+METRIC=0 # 0 for F, 1 for C
 
-temperature() {
-    xmllint --xpath 'string(//current/temperature/@value)' -
-}
+if [ -z $1 ] ; then
+  echo
+  echo "usage: weather.sh zip"
+  echo
+  exit 0
+fi
 
-icon() {
-    xmllint --xpath 'string(//current/weather/@icon)' -
-}
-
-
-main() {
-    xml=$(today $1)
-
-    case $(echo $xml | icon) in
-        #== day ==#
-        #
-        # clear sky
-        01d)
-            printf " "
-            ;;
-        # few-clouds
-        02d)
-            printf " "
-            ;;
-        # scattered-clouds
-        03d)
-            printf " "
-            ;;
-        # broken-clouds
-        04d)
-            printf " "
-            ;;
-        # shower rain
-        09d)
-            printf " "
-            ;;
-        # rain
-        10d)
-            printf " "
-            ;;
-        # thunderstorm
-        11d)
-            printf " "
-            ;;
-        # snow
-        13d)
-            printf " "
-            ;;
-        # mist
-        50d)
-            printf " "
-            ;;
-        #== night ==#
-        #
-        # clear sky
-        01n)
-            printf " "
-            ;;
-        # few-clouds
-        02n)
-            printf " "
-            ;;
-        # scattered-clouds
-        03n)
-            printf " "
-            ;;
-        # broken-clouds
-        04n)
-            printf " "
-            ;;
-        # shower rain
-        09n)
-            printf " "
-            ;;
-        # rain
-        10n)
-            printf " "
-            ;;
-        # thunderstorm
-        11n)
-            printf " "
-            ;;
-        # snow
-        13n)
-            printf " "
-            ;;
-        # mist
-        50n)
-            printf " "
-            ;;
-    esac
-
-    printf "%0.1f °F\n" $(echo $xml | temperature)
-}
-
-main $@
+curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1"; }'
+echo
--- bin/sh/weather3.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1"; }'
-echo
-
