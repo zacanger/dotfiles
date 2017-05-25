8a67012133d277fe55d337f0d3a75b96a5c0e1ad
zacanger <zac@zacanger.com>
2017-05-24 19:35:01 -0600

    cleanup

--- bin/js/tip-calculator.js
+++ bin/js/tip-calculator.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
+
 // usage:
 // ./tip-calculator.js price percentage
 // percentage defaults to 20
 
-let cost = process.argv[2]
-let percentage = process.argv[3] || 20
-cost = Number(cost)
-percentage = Number(percentage)
-const tip = cost * (percentage / 100)
+const cost = Number(process.argv[2])
+const perc = Number(process.argv[3] || 20)
+const tip = cost * (perc / 100)
 console.log(`
   Tip: ${tip.toFixed(2)}
   Total: ${(tip + cost).toFixed(2)}
