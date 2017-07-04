7807d0b858b27989561cded89dc9dd364c5c03b3
zacanger <zac@zacanger.com>
2017-07-04 14:06:45 -0600

    remove EVEN MORE YAY

--- bin/sh/dpaste.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-curl -sSi -F 'hold=on' -F 'content=<-' 'http://dpaste.com/api/v1/' \
-	| awk '$1=="Location:"{print $2}'
-
--- bin/sh/say.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# should say something (usage: say.sh something)
-(espeak || say || cat) << $1
-
