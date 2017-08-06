b594a7cc6ee12dc69c2d1c9913731bcda444ceab
zacanger <zac@zacanger.com>
2017-08-06 16:43:14 -0600

    remove more stuff, yay

--- bin/sh/my-top-history.awk
+++ bin/sh/my-top-history.awk
@@ -1,1 +1,2 @@
+#!/usr/bin/env awk
 history | awk '{a[$4]++}END{for(i in a){print a[i] " " i}}' | sort -rn | head
--- bin/sh/seller-status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-until [ `curl -s -o /dev/null -w "%{http_code}" https://seller.jane.com/-/diag` != '200' ]
-do
-  sleep 60
-done
-
-# npm i -g alert-node
-alert 'SELLER IS DOWN'
