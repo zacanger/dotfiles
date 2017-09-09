d01669ab1dfe031434be7fa2ce2edc0077285357
zacanger <zac@zacanger.com>
2017-09-08 23:33:02 -0600

    more fucking python, bye

--- bin/py/json2yaml.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-import json
-import yaml
-
-json_data = json.loads(open(sys.argv[1]).read())
-converted_json_data = json.dumps(json_data)
-print(yaml.dump(yaml.load(converted_json_data), default_flow_style=False))
