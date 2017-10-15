95df666a85287add1a578ff95764b8d94543870e
zacanger <zac@zacanger.com>
2017-10-15 10:39:22 -0600

    Rewrite flip text.

---
+++ bin/upsidedown.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-useless-escape */
+
+const normal = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,';0123456789?!"&()[]/\_‾=-<>`
+
+const flipped = `∀qƆpƎℲ⅁HIſʞ⅂WNOԀΌᴚS⊥∩ɅMX⅄Zɐqɔpǝɟ6ɥıſʞlɯuodbɹsʇnʌʍxʎz˙',؛0Ɩ2Ɛᔭ59Ɫ86¿¡„⅋)(][/\‾_=-><`
+
+const flip = (s) =>
+  s.split('').map((c) => normal.includes(c) ? flipped[normal.indexOf(c)] : c).reverse().join('')
+
+let text = ''
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+process.stdin.on('data', (chunk) => {
+  text += chunk
+})
+process.stdin.on('end', () => {
+  console.log(flip(text))
+})
