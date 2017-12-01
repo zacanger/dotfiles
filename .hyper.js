6dc773d486b11ee58e7fbc4ae24735f0f19c7f4d
zacanger <zac@zacanger.com>
2017-12-01 14:44:38 -0700

    rm more stuff

--- .hyper.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  config       : {fontSize : 12}
-, plugins      : [
-  'hypercwd'
-, 'hyperterm-mild-dark'
-]
-// put under $HOME/.hyperterm_plugins/local
-, localPlugins : []
-}
