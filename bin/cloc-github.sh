1b4c53bed17025b667d945be4a0499fac646c9f4
zacanger <zac@zacanger.com>
2018-01-07 15:10:40 -0700

    remove a thing and add a check for another thing

--- bin/cloc-github.sh
+++ bin/cloc-github.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if ! hash cloc 2>/dev/null ; then
+  echo 'please install cloc'
+  exit 1
+fi
+
 git clone --depth 1 "$1" temp-linecount-repo &&
   printf "('temp-linecount-repo' will be deleted automatically)\n\n\n" &&
   cloc temp-linecount-repo &&
