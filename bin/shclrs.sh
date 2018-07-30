9b58254550846a2a780db2809a12177b4d3d9567
zacanger <zac@zacanger.com>
2018-07-30 09:29:47 -0400

    bin: more

--- bin/shclrs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#shell foreground colors
-for i in `seq 30 37`;do echo -e "\033[${i}mForeground - ${i}";done
-
-#shell background colors
-for i in `seq 40 49`;do echo -e "\033[${i}mBackground - ${i}";done
