651801876568887f3ca97a2b1bac026679fb4368
zacanger <zac@zacanger.com>
2018-08-01 08:42:29 -0400

    bin: add screenlayout scripts

---
+++ bin/screenlayout-with-monitor.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+xrandr --output HDMI2 --mode 1920x1080 --pos 1600x0 --rotate normal --output HDMI1 --off --output DP1 --off --output eDP1 --primary --mode 1600x900 --pos 0x180 --rotate normal --output VIRTUAL1 --off
+wallpaper.sh
---
+++ bin/screenlayout-without-monitor.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+xrandr --output HDMI2 --off --output HDMI1 --off --output DP1 --off --output eDP1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+wallpaper.sh
