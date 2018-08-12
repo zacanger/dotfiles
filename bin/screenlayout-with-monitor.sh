162b24f07eb09c2c8aa027daeff19ba30a2fa943
zacanger <zac@zacanger.com>
2018-08-12 12:08:47 -0600

    new monitor

--- bin/screenlayout-with-monitor.sh
+++ bin/screenlayout-with-monitor.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-xrandr --output HDMI2 --mode 1920x1080 --pos 1600x0 --rotate normal --output HDMI1 --off --output DP1 --off --output eDP1 --primary --mode 1600x900 --pos 0x180 --rotate normal --output VIRTUAL1 --off
-wallpaper.sh
+xrandr --output HDMI2 --mode 3440x1440 --pos 1600x0 --rotate normal --output HDMI1 --off --output DP1 --off --output eDP1 --primary --mode 1600x900 --pos 0x540 --rotate normal --output VIRTUAL1 --off
