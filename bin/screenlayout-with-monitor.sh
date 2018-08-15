7f1e6831fe4e812baeacf439bc4c71824bb99666
zacanger <zac@zacanger.com>
2018-08-15 17:38:14 -0600

    bin: run wallpaper.sh on screenlayout

--- bin/screenlayout-with-monitor.sh
+++ bin/screenlayout-with-monitor.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 xrandr --output HDMI2 --mode 3440x1440 --pos 1600x0 --rotate normal --output HDMI1 --off --output DP1 --off --output eDP1 --primary --mode 1600x900 --pos 0x540 --rotate normal --output VIRTUAL1 --off
+
+wallpaper.sh
