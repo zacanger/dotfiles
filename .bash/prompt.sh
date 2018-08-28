0a954e8b2646d325fa6488dfc8e10c0ec80b4ced
zacanger <zac@zacanger.com>
2018-08-27 20:24:33 -0600

    bash: clean up prompt

--- .bash/prompt.sh
+++ .bash/prompt.sh
@@ -14,31 +14,30 @@ R='\[\e[1;38;5;196m\]'
 Y='\[\e[1;38;5;214m\]'
 W='\[\e[0m\]'
 
-get_prompt_symbol() {
+__get_prompt_symbol() {
   [[ $UID == 0 ]] && echo "#" || echo "\$"
 }
 
+USE_GIT_PROMPT=no
+
 if [[ $PS1 && -f /usr/share/git/git-prompt.sh ]]; then
   source /usr/share/git/git-prompt.sh
-  export GIT_PS1_SHOWDIRTYSTATE=1
-  export GIT_PS1_SHOWSTASHSTATE=1
-  export GIT_PS1_SHOWCOLORHINGS=1
-  export GIT_PS1_SHOWUPSTREAM='auto'
-  export GIT_PS1_SHOWUNTRACKEDFILES=1
-  export PROMPT_DIRTRIM=3
-  export PS1="$B\w\$(__git_ps1 \"$GY|$LB%s\")$GY $W\$(get_prompt_symbol) "
+  USE_GIT_PROMPT=yes
 elif [[ $PS1 && -f /usr/local/etc/bash_completion.d/git-prompt.sh ]]; then
-  # fuckin macs man
+  # on the work mac
   source /usr/local/etc/bash_completion.d/git-prompt.sh
+  USE_GIT_PROMPT=yes
+fi
+if [[ $USE_GIT_PROMPT == 'yes' ]]; then
   export GIT_PS1_SHOWDIRTYSTATE=1
   export GIT_PS1_SHOWSTASHSTATE=1
   export GIT_PS1_SHOWCOLORHINGS=1
   export GIT_PS1_SHOWUPSTREAM='auto'
   export GIT_PS1_SHOWUNTRACKEDFILES=1
   export PROMPT_DIRTRIM=3
-  export PS1="$B\w\$(__git_ps1 \"$GY|$LB%s\")$GY $W\$(get_prompt_symbol) "
+  export PS1="$B\w\$(__git_ps1 \"$GY|$LB%s\")$GY $W\$(__get_prompt_symbol) "
 else
-  export PS1="$B\w$GY $W\$(get_prompt_symbol) "
+  export PS1="$B\w$GY $W\$(__get_prompt_symbol) "
 fi
 
 unset B
@@ -50,3 +49,4 @@ unset PP
 unset R
 unset Y
 unset W
+unset USE_GIT_PROMPT
