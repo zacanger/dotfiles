7b4bd49042db1c72874ed9bd964978ded33070f4
zacanger <zac@zacanger.com>
2018-10-04 11:17:44 -0600

    bin: remove unknown flag in docker-fuck

--- bin/docker-fuck.sh
+++ bin/docker-fuck.sh
@@ -3,5 +3,5 @@
 docker kill `docker ps -q`
 docker rm -f `docker ps -aq`
 docker rmi -f `docker images -aq`
-docker network rm -f `docker network ls -q`
+docker network rm `docker network ls -q`
 docker volume rm -f `docker volume ls -q`
