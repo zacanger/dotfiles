8b566ee1f81bc5161500c3e0869b90e9e585c7af
zacanger <zac@zacanger.com>
2018-10-06 10:54:18 -0600

    bin: misc

--- bin/display-mirror-displayport.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# for mirroring my display when on displayport.
-xrandr --output DisplayPort-0 --auto --same-as LVDS
--- bin/display-mirror-vga.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# for setting my displays to mirror when plugged in with vga
-xrandr --output VGA-0 --auto --same-as LVDS
