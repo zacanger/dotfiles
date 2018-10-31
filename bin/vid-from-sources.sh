6ec3a49eb679d4ee48c955b53b5b73035cc9b60b
zacanger <zac@zacanger.com>
2018-10-30 22:16:51 -0600

    vid-from-sources.sh

---
+++ bin/vid-from-sources.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo audio
+read audio
+echo image
+read image
+echo output
+read output
+
+ffmpeg -loop 1 -i "$image" -i "$audio" -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest
+"$output"
