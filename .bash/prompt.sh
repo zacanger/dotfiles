126f43681cc1da69d0c9724650e9df9523b2dcbd
zacanger <zac@zacanger.com>
2018-11-10 09:35:30 -0700

    More prompt: sanctum geek's is nice

--- .bash/prompt.sh
+++ .bash/prompt.sh
@@ -1,130 +1,212 @@
-ESC=$'['
-END_ESC=m
+# Frontend to controlling prompt
+prompt() {
 
-NOPRINT='\['
-END_NOPRINT='\]'
+    # What's done next depends on the first argument to the function
+    case $1 in
 
-WRAP="$NOPRINT$ESC"
-END_WRAP="$END_ESC$END_NOPRINT"
+        # Turn complex, colored PS1 and debugging PS4 prompts on
+        on)
+            # Set up pre-prompt command
+            PROMPT_COMMAND='history -a'
 
-RESET="${WRAP}0${END_WRAP}"
-RESET_BG="${WRAP}49${END_WRAP}"
+            # If Bash 4.0 is available, trim very long paths in prompt
+            ((BASH_VERSINFO[0] >= 4)) && PROMPT_DIRTRIM=4
 
-NORMAL="${WRAP}21${END_WRAP}"
-BOLD="${WRAP}1${END_WRAP}"
+            # Basic prompt shape depends on whether we're in SSH or not
+            PS1=
+            if [[ -n $SSH_CLIENT || -n $SSH_CONNECTION ]] ; then
+                PS1=$PS1'\h:'
+            fi
+            PS1=$PS1'\w'
 
-function __fg_color {
-  printf "%s" "${WRAP}38;5;${1}${END_WRAP}"
+            # Add sub-commands; VCS, job, and return status checks
+            PS1=$PS1'$(ret=$?;prompt vcs;prompt job;prompt ret)'
+
+            # Add a helpful prefix if this shell appears to be exotic
+            case ${SHELL##*/} in
+                (bash) ;;
+                (*) PS1=bash:$PS1 ;;
+            esac
+
+            # Add prefix and suffix
+            PS1='${PROMPT_PREFIX}'$PS1'${PROMPT_SUFFIX}'
+
+            # Add terminating "$" or "#" sign
+            PS1=$PS1'\$'
+
+            # Declare variables to contain terminal control strings
+            local format reset
+
+            # Disregard output and error from these tput(1) calls
+            {
+                # Count available colors
+                local -i colors
+                colors=$(tput colors || tput Co)
+
+                # Prepare reset code
+                reset=$(tput sgr0 || tput me)
+
+                # Check if we have non-bold bright green available
+                if ((colors >= 16)) ; then
+                    format=$(
+                        pc=${PROMPT_COLOR:-10}
+                        tput setaf "$pc" ||
+                        tput setaf "$pc" 0 0 ||
+                        tput AF "$pc" ||
+                        tput AF "$pc" 0 0
+                    )
+
+                # If we have only eight colors, use bold green
+                elif ((colors >= 8)) ; then
+                    format=$(
+                        pc=${PROMPT_COLOR:-2}
+                        tput setaf "$pc" || tput AF "$pc"
+                        tput bold || tput md
+                    )
+
+                # Otherwise, we just try bold
+                else
+                    format=$(tput bold || tput md)
+                fi
+
+            } >/dev/null 2>&1
+
+            # String it all together
+            PS1='\['"$format"'\]'"$PS1"'\['"$reset"'\] '
+            PS2='> '
+            PS3='? '
+            PS4='+<$?> ${BASH_SOURCE:-$BASH}:${FUNCNAME[0]}:$LINENO:'
+            ;;
+
+        # Revert to simple inexpensive prompts
+        off)
+            unset -v PROMPT_COMMAND PROMPT_DIRTRIM
+            PS1='$ '
+            PS2='> '
+            PS3='? '
+            PS4='+ '
+            if [[ -n $SSH_CLIENT || -n $SSH_CONNECTION ]] ; then
+                PS1=$(hostname -s)'$ '
+            fi
+            ;;
+
+        # Git prompt function
+        git)
+
+            # Wrap as compound command; we don't want to see output from any of
+            # these git(1) calls
+            {
+                # Bail if we're not in a work tree--or, implicitly, if we don't
+                # have git(1).
+                [[ $(git rev-parse --is-inside-work-tree) == true ]] ||
+                    return
+
+                # Refresh index so e.g. git-diff-files(1) is accurate
+                git update-index --refresh
+
+                # Find a local branch, remote branch, or tag (annotated or
+                # not), or failing all of that just show the short commit ID,
+                # in that order of preference; if none of that works, bail out
+                local name
+                name=$(
+                    git symbolic-ref --quiet HEAD ||
+                    git describe --tags --exact-match HEAD ||
+                    git rev-parse --short HEAD
+                ) || return
+                name=${name#refs/*/}
+                [[ -n $name ]] || return
+
+                # Check various files in .git to flag processes
+                local proc
+                [[ -d .git/rebase-merge || -d .git/rebase-apply ]] &&
+                    proc=${proc:+"$proc",}'REBASE'
+                [[ -f .git/MERGE_HEAD ]] &&
+                    proc=${proc:+"$proc",}'MERGE'
+                [[ -f .git/CHERRY_PICK_HEAD ]] &&
+                    proc=${proc:+"$proc",}'PICK'
+                [[ -f .git/REVERT_HEAD ]] &&
+                    proc=${proc:+"$proc",}'REVERT'
+                [[ -f .git/BISECT_LOG ]] &&
+                    proc=${proc:+"$proc",}'BISECT'
+
+                # Collect symbols representing repository state
+                local state
+
+                # Upstream HEAD has commits after local HEAD; we're "behind"
+                (($(git rev-list --count 'HEAD..@{u}'))) &&
+                    state=${state}'<'
+
+                # Local HEAD has commits after upstream HEAD; we're "ahead"
+                (($(git rev-list --count '@{u}..HEAD'))) &&
+                    state=${state}'>'
+
+                # Tracked files are modified
+                git diff-files --no-ext-diff --quiet ||
+                    state=${state}'!'
+
+                # Changes are staged
+                git diff-index --cached --no-ext-diff --quiet HEAD ||
+                    state=${state}'+'
+
+                # There are some untracked and unignored files
+                git ls-files --directory --error-unmatch --exclude-standard \
+                    --no-empty-directory --others -- ':/*' &&
+                    state=${state}'?'
+
+                # There are stashed changes
+                git rev-parse --quiet --verify refs/stash &&
+                    state=${state}'^'
+
+            } >/dev/null 2>&1
+
+            # For some reason, five commands in the above block seem to stick
+            # around as jobs after this command is over; I don't know why, but
+            # this clears it; might be a bug
+            jobs >/dev/null
+
+            # Print the status in brackets; add a git: prefix only if there
+            # might be another VCS prompt (because PROMPT_VCS is set)
+            printf '(%s%s%s%s)' \
+                "${PROMPT_VCS:+git:}" \
+                "${name//\\/\\\\}" \
+                "${proc:+:"${proc//\\/\\\\}"}" \
+                "${state//\\/\\\\}"
+            ;;
+
+        # VCS wrapper prompt function; print the first relevant prompt, if any
+        vcs)
+            local vcs
+            for vcs in "${PROMPT_VCS[@]:-git}" ; do
+                prompt "$vcs" && return
+            done
+            ;;
+
+        # Show return status of previous command in angle brackets, if not zero
+        ret)
+            # shellcheck disable=SC2154
+            ((ret)) && printf '<%u>' "${ret//\\/\\\\}"
+            ;;
+
+        # Show the count of background jobs in curly brackets, if not zero
+        job)
+            local -i jobc
+            while read -r ; do
+                ((jobc++))
+            done < <(jobs -p)
+            ((jobc)) && printf '{%u}' "${jobc//\\/\\\\}"
+            ;;
+
+        # No argument given, print prompt strings and vars
+        '')
+            declare -p PS1 PS2 PS3 PS4
+            ;;
+
+        # Print error
+        *)
+            printf '%s: Unknown command %s\n' "${FUNCNAME[0]}" "$1" >&2
+            return 2
+            ;;
+    esac
 }
 
-function __bg_color {
-  printf "%s" "${WRAP}48;5;${1}${END_WRAP}"
-}
-
-BLACK=$(__fg_color 0)
-RED=$(__fg_color 1)
-GREEN=$(__fg_color 2)
-YELLOW=$(__fg_color 3)
-BLUE=$(__fg_color 4)
-PURPLE=$(__fg_color 5)
-CYAN=$(__fg_color 6)
-WHITE=$(__fg_color 7)
-
-ALT_BLACK=$(__fg_color 8)
-ALT_RED=$(__fg_color 9)
-ALT_GREEN=$(__fg_color 10)
-ALT_YELLOW=$(__fg_color 11)
-ALT_BLUE=$(__fg_color 12)
-ALT_PURPLE=$(__fg_color 13)
-ALT_CYAN=$(__fg_color 14)
-ALT_WHITE=$(__fg_color 15)
-
-BLACK_BG=$(__bg_color 0)
-RED_BG=$(__bg_color 1)
-GREEN_BG=$(__bg_color 2)
-YELLOW_BG=$(__bg_color 3)
-BLUE_BG=$(__bg_color 4)
-PURPLE_BG=$(__bg_color 5)
-CYAN_BG=$(__bg_color 6)
-WHITE_BG=$(__bg_color 7)
-
-ALT_BLACK_BG=$(__bg_color 8)
-ALT_RED_BG=$(__bg_color 9)
-ALT_GREEN_BG=$(__bg_color 10)
-ALT_YELLOW_BG=$(__bg_color 11)
-ALT_BLUE_BG=$(__bg_color 12)
-ALT_PURPLE_BG=$(__bg_color 13)
-ALT_CYAN_BG=$(__bg_color 14)
-ALT_WHITE_BG=$(__bg_color 15)
-
-function __parse_git_branch() {
-  local branch stat
-
-  if branch=$( { git symbolic-ref --quiet HEAD || git rev-parse --short HEAD; } 2>/dev/null ); then
-    stat="$(__parse_git_dirty)"
-    printf "%s" "${branch##*/}${stat}"
-    return
-  fi
-
-  return 1
-}
-
-function __parse_git_dirty {
-  local status dirty untracked ahead newfile renamed deleted bits
-
-  status=`git status 2>&1 | tee`
-  dirty=`echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?"`
-  untracked=`echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?"`
-  ahead=`echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?"`
-  newfile=`echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?"`
-  renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
-  deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
-  bits=''
-  if [ "${renamed}" == "0" ]; then
-    bits=">${bits}"
-  fi
-  if [ "${ahead}" == "0" ]; then
-    bits="*${bits}"
-  fi
-  if [ "${newfile}" == "0" ]; then
-    bits="+${bits}"
-  fi
-  if [ "${untracked}" == "0" ]; then
-    bits="?${bits}"
-  fi
-  if [ "${deleted}" == "0" ]; then
-    bits="x${bits}"
-  fi
-  if [ "${dirty}" == "0" ]; then
-    bits="!${bits}"
-  fi
-
-  if [ ! "${bits}" == "" ]; then
-    printf "%s" " ${bits}"
-  fi
-}
-
-function __prompt_ps1 {
-  printf "%s" "${NORMAL}\w "
-
-  local stat
-  if stat=$(__parse_git_branch); then
-    printf "%s" "${stat} "
-  fi
-
-  printf "%s" "${RESET_BG}$(__fg_color 241)"
-  printf "%s" "${RESET}${RESET_BG}"
-}
-
-function __prompt {
-  PS1="$(__prompt_ps1)"
-}
-
-if [[ ! "$PROMPT_COMMAND" == *__prompt* ]]; then
-  case $(tty) in
-    /dev/tty[0-9]*)
-      ;;
-    *)
-      PROMPT_COMMAND='__prompt;'$'\n'"$PROMPT_COMMAND" ;;
-  esac
-fi
+prompt "${PROMPT_MODE:-on}"
