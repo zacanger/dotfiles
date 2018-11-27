ed933c26cde184b924c9286043d44f08e7cde349
zacanger <zac@zacanger.com>
2018-11-26 19:19:57 -0700

    bin: names cleanup

--- bin/names.sh
+++ bin/names.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# convert filenames to lowercase and replace characters recursively
-
 if [ -z "$1" ]; then
-  echo target directory please
+  echo 'names: convert filenames to lowercase and replace characters, recursively'
+  echo 'usage: names path/to/directory'
   exit 0
 fi
 
@@ -15,11 +14,6 @@ find "$1" -depth -name '*' | while read file ; do
   if [ "$oldfilename" != "$newfilename" ]; then
     mv -i "$directory/$oldfilename" "$directory/$newfilename"
     echo ""$directory/$oldfilename" ---> "$directory/$newfilename""
-    #echo "$directory"
-    #echo "$oldfilename"
-    #echo "$newfilename"
-    #echo
-    #tr 'A-Z' 'a-z'
   fi
 done
 exit 0
