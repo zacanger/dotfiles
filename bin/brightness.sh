6263ef244df846f23b79f808ce155df2b3d494f1
zacanger <zac@zacanger.com>
2018-12-07 20:17:50 -0700

    brightness.sh

--- bin/brightness.sh
+++ bin/brightness.sh
@@ -6,9 +6,9 @@ backlight_path=/sys/class/backlight/intel_backlight/brightness
 arg="$1"
 
 if [ "$arg" = 'low' ]; then
-  echo 2000 > $backlight_path
+  echo 500 > $backlight_path
 elif [ "$arg" = 'mid' ]; then
-  echo 4000 > $backlight_path
+  echo 2500 > $backlight_path
 else
   echo 6000 > $backlight_path
 fi
