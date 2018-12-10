a974a8807fc7afc5914ea5dbbc3e1f5c4712cd3e
zacanger <zac@zacanger.com>
2018-12-10 11:38:10 -0700

    bin/lock.sh

--- bin/lock.sh
+++ bin/lock.sh
@@ -3,6 +3,11 @@
 if [[ `uname` == 'Darwin' ]]; then
   /System/Library/CoreServices/"Menu Extras"/User.menu/Contents/Resources/CGSession -suspend
 else
-  i3lock --color=212121
-  exit 0
+  if hash i3lock 2>/dev/null; then
+    i3lock --color=212121
+  elif hash xtrlock 2>/dev/null; then
+    xtrlock -b
+  elif hash slock 2>/dev/null; then
+    slock
+  fi
 fi
