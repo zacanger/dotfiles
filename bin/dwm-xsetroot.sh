40f87b8c9db6f32136bd1eccc1d98c9cdad71c79
zacanger <zac@zacanger.com>
2018-12-10 11:49:36 -0700

    bin/dwm-xsetroot.sh

--- bin/dwm-xsetroot.sh
+++ bin/dwm-xsetroot.sh
@@ -3,6 +3,6 @@
 while true; do
   batt="$(cat /sys/class/power_supply/BAT0/capacity)"
   disk="$(df -h / | awk '{print $4}' | tail -1)"
-  xsetroot -name "$(date) batt: $batt disk: $disk"
+  xsetroot -name "batt: $batt | disk: $disk | $(date)"
   sleep 10
 done
