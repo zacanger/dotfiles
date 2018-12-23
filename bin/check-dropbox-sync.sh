cc97439cb0c338852803370a044eb9cf8757d021
zacanger <zac@zacanger.com>
2018-12-22 17:14:04 -0700

    check dropbox status

--- bin/check-dropbox-sync.sh
+++ bin/check-dropbox-sync.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-while true
-do
+if [[ `uname -a` == *"Arch"* ]]; then
+  dropbox_cmd=dropbox-cli
+else
+  dropbox_cmd=dropbox
+fi
+while true; do
   sleep 10
   clear
-  dropbox-cli status
+  $dropbox_cmd status
 done
