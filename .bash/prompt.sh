6d3ef4e04345b47d14e22b5d82c112f1fffaa542
zacanger <zac@zacanger.com>
2018-12-25 21:35:48 -0700

    Try a prompt

--- .bash/prompt.sh
+++ .bash/prompt.sh
@@ -1,114 +1,99 @@
-prompt() {
-  case $1 in
-    on)
-      PROMPT_COMMAND='history -a'
-      PROMPT_DIRTRIM=2
+# Reset
+Color_Off="\[\033[0m\]"       # Text Reset
 
-      PS1=
-      PS1=$PS1'\w'
-      PS1=$PS1'$(prompt git)'
+# Regular Colors
+Black="\[\033[0;30m\]"        # Black
+Red="\[\033[0;31m\]"          # Red
+Green="\[\033[0;32m\]"        # Green
+Yellow="\[\033[0;33m\]"       # Yellow
+Blue="\[\033[0;34m\]"         # Blue
+Purple="\[\033[0;35m\]"       # Purple
+Cyan="\[\033[0;36m\]"         # Cyan
+White="\[\033[0;37m\]"        # White
 
-      # Add a helpful prefix if this shell appears to be exotic
-      case ${SHELL##*/} in
-        (bash) ;;
-        (*) PS1=bash:$PS1 ;;
-      esac
+# Bold
+BBlack="\[\033[1;30m\]"       # Black
+BRed="\[\033[1;31m\]"         # Red
+BGreen="\[\033[1;32m\]"       # Green
+BYellow="\[\033[1;33m\]"      # Yellow
+BBlue="\[\033[1;34m\]"        # Blue
+BPurple="\[\033[1;35m\]"      # Purple
+BCyan="\[\033[1;36m\]"        # Cyan
+BWhite="\[\033[1;37m\]"       # White
 
-      # Add prefix and suffix
-      PS1='${PROMPT_PREFIX}'$PS1'${PROMPT_SUFFIX}'
+# Underline
+UBlack="\[\033[4;30m\]"       # Black
+URed="\[\033[4;31m\]"         # Red
+UGreen="\[\033[4;32m\]"       # Green
+UYellow="\[\033[4;33m\]"      # Yellow
+UBlue="\[\033[4;34m\]"        # Blue
+UPurple="\[\033[4;35m\]"      # Purple
+UCyan="\[\033[4;36m\]"        # Cyan
+UWhite="\[\033[4;37m\]"       # White
 
-      # Declare variables to contain terminal control strings
-      local format reset
+# Background
+On_Black="\[\033[40m\]"       # Black
+On_Red="\[\033[41m\]"         # Red
+On_Green="\[\033[42m\]"       # Green
+On_Yellow="\[\033[43m\]"      # Yellow
+On_Blue="\[\033[44m\]"        # Blue
+On_Purple="\[\033[45m\]"      # Purple
+On_Cyan="\[\033[46m\]"        # Cyan
+On_White="\[\033[47m\]"       # White
 
-      # Disregard output and error from these tput(1) calls
-      {
-        reset=$(tput sgr0 || tput me)
-        format=$(
-          pc=${PROMPT_COLOR:-10}
-          tput setaf "$pc" ||
-          tput setaf "$pc" 0 0 ||
-          tput AF "$pc" ||
-          tput AF "$pc" 0 0
-        )
-      } >/dev/null 2>&1
+# High Intensty
+IBlack="\[\033[0;90m\]"       # Black
+IRed="\[\033[0;91m\]"         # Red
+IGreen="\[\033[0;92m\]"       # Green
+IYellow="\[\033[0;93m\]"      # Yellow
+IBlue="\[\033[0;94m\]"        # Blue
+IPurple="\[\033[0;95m\]"      # Purple
+ICyan="\[\033[0;96m\]"        # Cyan
+IWhite="\[\033[0;97m\]"       # White
 
-      # String it all together
-      PS1='\['"$format"'\]'"$PS1"'\['"$reset"'\] '
-      PS2='> '
-      PS3='? '
-      PS4='+<$?> ${BASH_SOURCE:-$BASH}:${FUNCNAME[0]}:$LINENO:'
-      ;;
+# Bold High Intensty
+BIBlack="\[\033[1;90m\]"      # Black
+BIRed="\[\033[1;91m\]"        # Red
+BIGreen="\[\033[1;92m\]"      # Green
+BIYellow="\[\033[1;93m\]"     # Yellow
+BIBlue="\[\033[1;94m\]"       # Blue
+BIPurple="\[\033[1;95m\]"     # Purple
+BICyan="\[\033[1;96m\]"       # Cyan
+BIWhite="\[\033[1;97m\]"      # White
 
-    git)
-      # Wrap as compound command; we don't want to see output from any of these calls
-      {
-        # Bail if we're not in a work tree - or, implicitly, if we don't have git
-        [[ $(git rev-parse --is-inside-work-tree) == true ]] || return
+# High Intensty backgrounds
+On_IBlack="\[\033[0;100m\]"   # Black
+On_IRed="\[\033[0;101m\]"     # Red
+On_IGreen="\[\033[0;102m\]"   # Green
+On_IYellow="\[\033[0;103m\]"  # Yellow
+On_IBlue="\[\033[0;104m\]"    # Blue
+On_IPurple="\[\033[10;95m\]"  # Purple
+On_ICyan="\[\033[0;106m\]"    # Cyan
+On_IWhite="\[\033[0;107m\]"   # White
 
-        # Refresh index so e.g. git-diff-files(1) is accurate
-        git update-index --refresh
+# Various variables you might want for your PS1 prompt instead
+PathShort="\w"
+PathFull="\W"
+NewLine="\n"
+Jobs="\j"
 
-        # Find a local branch, remote branch, or tag (annotated or not), or failing all of that just show the short
-        # commit ID, in that order of preference; if none of that works, bail out
-        local name
-        name=$(
-          git symbolic-ref --quiet HEAD ||
-          git describe --tags --exact-match HEAD ||
-          git rev-parse --short HEAD
-        ) || return
-        name=${name#refs/*/}
-        [[ -n $name ]] || return
+if [[ $PS1 && -f /usr/share/git/git-prompt.sh ]]; then
+  source /usr/share/git/git-prompt.sh
+elif [[ $PS1 && -f /usr/local/etc/bash_completion.d/git-prompt.sh ]]; then
+  source /usr/local/etc/bash_completion.d/git-prompt.sh
+fi
 
-        # Check various files in .git to flag processes
-        local proc
-        [[ -d .git/rebase-merge || -d .git/rebase-apply ]] && proc=${proc:+"$proc",}'REBASE'
-        [[ -f .git/MERGE_HEAD ]] && proc=${proc:+"$proc",}'MERGE'
-        [[ -f .git/CHERRY_PICK_HEAD ]] && proc=${proc:+"$proc",}'PICK'
-        [[ -f .git/REVERT_HEAD ]] && proc=${proc:+"$proc",}'REVERT'
-        [[ -f .git/BISECT_LOG ]] && proc=${proc:+"$proc",}'BISECT'
-
-        # Collect symbols representing repository state
-        local state
-
-        # Upstream HEAD has commits after local HEAD; we're "behind"
-        (($(git rev-list --count 'HEAD..@{u}'))) && state=${state}'<'
-
-        # Local HEAD has commits after upstream HEAD; we're "ahead"
-        (($(git rev-list --count '@{u}..HEAD'))) && state=${state}'>'
-
-        # Tracked files are modified
-        git diff-files --no-ext-diff --quiet || state=${state}'!'
-
-        # Changes are staged
-        git diff-index --cached --no-ext-diff --quiet HEAD || state=${state}'+'
-
-        # There are some untracked and unignored files
-        git ls-files --directory --error-unmatch --exclude-standard \
-            --no-empty-directory --others -- ':/*' &&
-            state=${state}'?'
-
-        # There are stashed changes
-        git rev-parse --quiet --verify refs/stash && state=${state}'^'
-      } >/dev/null 2>&1
-
-      # For some reason, five commands in the above block seem to stick around as jobs after this command is over; I
-      # don't know why, but this clears it; might be a bug
-      jobs >/dev/null
-
-      # Print the status in brackets; add a git: prefix only if there might be another VCS prompt (because PROMPT_VCS is
-      # set)
-      printf '(%s%s%s%s)' \
-        "${PROMPT_VCS:+git:}" \
-        "${name//\\/\\\\}" \
-        "${proc:+:"${proc//\\/\\\\}"}" \
-        "${state//\\/\\\\}"
-      ;;
-
-    *)
-      printf '%s: Unknown command %s\n' "${FUNCNAME[0]}" "$1" >&2
-      return 2
-      ;;
-  esac
-}
-
-prompt "${PROMPT_MODE:-on}"
+export PS1='$(git branch &>/dev/null;\
+if [ $? -eq 0 ]; then \
+  echo "$(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
+  if [ "$?" -eq "0" ]; then \
+    # @4 - Clean repository - nothing to commit
+    echo "'$Green'"$(__git_ps1 " (%s)"); \
+  else \
+    # @5 - Changes to working tree
+    echo "'$IRed'"$(__git_ps1 " {%s}"); \
+  fi) '$BYellow$PathShort$Color_Off' \$ "; \
+else \
+  # @2 - Prompt when not in GIT repo
+  echo " '$Yellow$PathShort$Color_Off' \$ "; \
+fi)'
