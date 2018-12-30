3c9c0fa17091428eff9101efa10772ec7aa29b31
zacanger <zac@zacanger.com>
2018-12-29 18:21:17 -0700

    wmjs config

--- .config/wmjs.js
+++ .config/wmjs.js
@@ -1,14 +1,11 @@
 module.exports = () => ({
   startupPrograms: [
-    // 'compton -b',
-    // 'wallpaper.sh',
-    // 'xflux -z 84047',
+    'wallpaper.sh',
+    'xflux -z 84047',
     'setxkbmap -option "caps:swapescape"'
   ],
-  borderWidth: 4,
+  borderWidth: 2,
   borderColor: '00ff00',
-  // launcher: 'dmenu_recency',
   launcher: 'dmenu_run',
-  // terminal: 'zt'
-  terminal: 'xterm'
+  terminal: 'st'
 })
