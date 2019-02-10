8c383a41148968c5009977e00ce6a371821f1821
zacanger <zac@zacanger.com>
2019-02-09 21:49:49 -0700

    fix vid-from-sources

--- bin/vid-from-sources.sh
+++ bin/vid-from-sources.sh
@@ -7,5 +7,4 @@ read image
 echo output
 read output
 
-ffmpeg -loop 1 -i "$image" -i "$audio" -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest
-"$output"
+ffmpeg -loop 1 -i "$image" -i "$audio" -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest "$output"
