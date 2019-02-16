0832561935578a2109df71ff4995bd85f94c8ddc
zacanger <zac@zacanger.com>
2019-02-15 19:50:46 -0700

    adjust some scripts

--- bin/new-linux.sh
+++ bin/new-linux.sh
@@ -130,3 +130,15 @@ nvim +PlugInstall +qa
 # link fonts
 ln -s $z_path/x/fonts/ $HOME/.local/share/
 fc-cache
+
+# install docker if deb/ubuntu
+if [[ `uname -a` == *"Debian"* ]] || [[ `uname -a` == *"Ubuntu"* ]]; then
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+  sudo usermod -aG docker $USER
+fi
