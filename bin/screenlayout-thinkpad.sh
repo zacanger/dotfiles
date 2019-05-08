b5604d5ea2aa0a28f116ba649332162827b3cd63
zacanger <zac@zacanger.com>
2019-05-08 15:08:50 -0600

    new screenlayout xrandr script

--- bin/screenlayout-thinkpad.sh
+++ bin/screenlayout-thinkpad.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output LVDS-1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off
+xrandr --output LVDS-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output VGA-1-2 --mode 1920x1080 --pos 0x0 --rotate normal
