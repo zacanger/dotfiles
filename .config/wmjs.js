9d49fa3d4466cff9a823cf31a2c54d3cab942051
zacanger <zac@zacanger.com>
2019-05-22 13:13:46 -0600

    wmjs config

--- .config/wmjs.js
+++ .config/wmjs.js
@@ -1,12 +1,11 @@
 module.exports = () => ({
   startupPrograms: [
-    'wallpaper.sh',
     'redshift -l 40.760780:-111.891045',
     'setxkbmap -option "caps:swapescape"'
   ],
   borderWidth: 1,
   borderColor: '9CD7A1',
-  launcher: 'dmenu_run',
-  terminal: 'st',
+  launcher: 'rofi',
+  terminal: 'xterm',
   log: true
 })
