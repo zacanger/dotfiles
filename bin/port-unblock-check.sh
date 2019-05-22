dd1b7cede6bf5044401112259ea17ec4ec77fdc3
zacanger <zac@zacanger.com>
2019-05-21 20:50:02 -0600

    misc

--- bin/port-unblock-check.sh
+++ bin/port-unblock-check.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-for i in {1..65535}
-do
+for i in {1..65535}; do
   nc -w 2 open.zorinaq.com $i
   rc=$?
   if [[ $rc == 0 ]] ; then
--- bin/portscanner.js
+++ bin/portscanner.js
@@ -3,9 +3,10 @@
 const net = require('net')
 const host = process.argv[2] || 'localhost'
 const end = 50000
-const timeout = 2000
 // we want the sockets to time out quickly so as not to waste
 // resources, but not _too_ quickly, so we don't miss open sockets.
+const timeout = 2000
+
 let start = 1
 
 while (start <= end) {
