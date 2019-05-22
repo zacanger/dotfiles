3a48ca1c2db1a5c012247e5b5ed1c56a4a9b94d9
zacanger <zac@zacanger.com>
2019-05-22 12:26:18 -0600

    rofi colors

--- bin/rofi-run.sh
+++ bin/rofi-run.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-rofi -show run -modi run -location 7 -width 25 \
-  -lines 1 -line-margin 0 -line-padding 1 \
-  -separator-style none -font "Hack 10" -columns 2 -bw 0 \
+rofi \
+  -show run \
+  -modi run \
+  -location 7 \
+  -width 25 \
+  -lines 1 \
+  -line-margin 0 \
+  -line-padding 1 \
+  -separator-style none \
+  -font "Hack 10" \
+  -columns 2 \
+  -bw 0 \
   -disable-history \
   -hide-scrollbar \
-  -color-window "#222222, #222222, #b1b4b3" \
-  -color-normal "#222222, #b1b4b3, #222222, #005577, #b1b4b3" \
-  -color-active "#222222, #b1b4b3, #222222, #007763, #b1b4b3" \
-  -color-urgent "#222222, #b1b4b3, #222222, #77003d, #b1b4b3" \
-  -kb-row-select "Tab" -kb-row-tab ""
+  -color-window "#3d3d3d, #3d3d3d, #b1b4b3" \
+  -color-normal "#3d3d3d, #d2d8cf, #3d3d3d, #a2a497, #fafbf8" \
+  -color-active "#3d3d3d, #d2d8cf, #3d3d3d, #d2d8cf, #fafbf8" \
+  -color-urgent "#3d3d3d, #d2d8cf, #3d3d3d, #77003d, #fafbf8" \
+  -kb-row-select "Tab" \
+  -kb-row-tab ""
