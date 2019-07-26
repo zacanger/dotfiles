9ce6746eeb099a20230720f98360b2bbc33f0200
zacanger <zac@zacanger.com>
2019-07-26 10:59:48 -0600

    bin cleanup

--- bin/docker-auto-compose.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-for containerid in `docker ps --format "{{.Names}}"`; do
-  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock red5d/docker-autocompose $containerid >> autocompose.txt
-done
