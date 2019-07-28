68648649210c06232d8bb1a97c2311c6b0531919
zacanger <zac@zacanger.com>
2019-07-28 10:51:12 -0600

    add gcloud to new-linux setup

--- bin/new-linux.sh
+++ bin/new-linux.sh
@@ -129,3 +129,9 @@ curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb
 sudo dpkg -i keybase_amd64.deb
 sudo apt-get install -f
 run_keybase
+
+# set up gcloud cli
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+gcloud init
