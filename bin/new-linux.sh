4e588b3efcbfabd627df5fd68c4a5f64a374faef
zacanger <zac@zacanger.com>
2019-09-14 20:15:53 -0600

    bin/new-linux.sh

--- bin/new-linux.sh
+++ bin/new-linux.sh
@@ -127,3 +127,8 @@ echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.clou
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk -y
 gcloud init
+
+# cleanup
+apt autoremove
+apt purge
+apt clean
