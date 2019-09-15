bfdcb1156f81bc431ecdb9273ab9e28084c7d5fe
zacanger <zac@zacanger.com>
2019-09-14 20:24:15 -0600

    new-linux

--- bin/new-linux.sh
+++ bin/new-linux.sh
@@ -105,7 +105,6 @@ sudo add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) \
   stable"
-sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
 sudo usermod -aG docker $USER
 
