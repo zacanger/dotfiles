ce08368171efaf8f777ec737132e07bf88c28e98
zacanger <zac@zacanger.com>
2019-09-15 18:12:32 -0600

    chore: add pcmanfm conf

--- bin/new-linux.sh
+++ bin/new-linux.sh
@@ -82,6 +82,7 @@ $zconf_path=$z_path/.config
 mkdir -p $conf_path/i3
 mkdir -p $conf_path/i3status
 mkdir -p $conf_path/ranger
+mkdir -p $conf_path/pcmanfm/default
 ln -s $zconf_path/ranger/rc.conf $conf_path/ranger/
 ln -s $zconf_path/ranger/rifle.conf $conf_path/ranger/
 ln -s $zconf_path/ranger/scope.sh $conf_path/ranger/
@@ -90,6 +91,9 @@ ln -s $zconf_path/i3status/config $conf_path/i3status/
 ln -s $zconf_path/ninit $conf_path/
 ln -s $zconf_path/nvim $conf_path/
 ln -s $zconf_path/startup.py $conf_path/
+
+# copy rather than link because config contains window height
+cp $zconf_path/pcmanfm/default/pcmanfm.conf $conf_path/pcmanfm/default/
 
 # install vim shit
 nvim +PlugInstall +qa
