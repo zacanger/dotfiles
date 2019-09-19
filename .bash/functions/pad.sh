b98bd7b6882b4f28fe61ccb2cfee342c5653a539
zacanger <zac@zacanger.com>
2019-09-18 20:27:10 -0600

    chore: rm more fns

--- .bash/functions/pad.sh
+++
@@ -1,3 +0,0 @@
-pad() {
-  for (( i = 0; i < ${2:-0}; i++ )); do printf "${1:- }"; done
-}
--- .bash/functions/pan.sh
+++
@@ -1,7 +0,0 @@
-# print args, null-delimited. probably want to use in a file,
-# or as part of a pipeline. see also: `pa()`
-pan() {
-  if (($#)); then
-    printf '%s\0' "$@"
-  fi
-}
