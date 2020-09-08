183fbd007df08e69ccd8fe41fcdb87069a5989d8
zacanger <zac@zacanger.com>
2020-09-07 20:30:25 -0600

    chore: flip ctof around

--- bin/ctof.sh
+++ bin/ctof.sh
@@ -1,33 +1,34 @@
 #!/usr/bin/env bash
+set -e
 
 main_menu() {
-  echo '1) Fahrenheit to Celsius'
-  echo '2) Celsius to Fahrenheit'
+  echo '1) Celsius to Fahrenheit'
+  echo '2) Fahrenheit to Celsius'
   echo ""
   read -rp "Please choose one of the options above: " option
   case $option in
-    1) F2C;
+    1) c_to_f
       ;;
-    2) C2F
+    2) f_to_c
       ;;
     *) echo "Invalid choice!"; exit 1
       ;;
   esac
 }
 
-F2C() {
+f_to_c() {
   echo "Fahrenheit to Celsius Conversion"
-  read -rp "Enter the temperature (in Fahrenheit): " FT
-  tconv=$(echo "($FT - 32) * 5 / 9 " | bc)
+  read -rp "Enter the temperature (in Fahrenheit): " f_temp
+  tconv=$(echo "($f_temp - 32) * 5 / 9 " | bc)
   echo -en "The Temperature in Celsius is "
   echo "$tconv"
   exit 0
 }
 
-C2F() {
+c_to_f() {
   echo "Celsius to Fahrenheit Conversion"
-  read -rp "Enter the temperature (in Celsius): " CT
-  tconv=$(echo "$CT * 9 / 5 + 32 " | bc)
+  read -rp "Enter the temperature (in Celsius): " c_temp
+  tconv=$(echo "$c_temp * 9 / 5 + 32 " | bc)
   echo -en "The Temperature in Fahrenheit is "
   echo "$tconv"
   exit 0
