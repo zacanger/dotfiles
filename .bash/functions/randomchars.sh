85c8a32f37393794c9ae5c71f96bc77456935879
zacanger <zac@zacanger.com>
2020-10-23 10:07:49 -0600

    chore: better randomchars

---
+++ .bash/functions/randomchars.sh
@@ -0,0 +1,16 @@
+# shellcheck shell=bash
+
+randomchars() {
+  one=$(dd if=/dev/urandom count=1 2> /dev/null \
+    | uuencode - \
+    | sed -ne 2p \
+    | cut -c-10)
+
+  two=$(dd if=/dev/urandom count=1 2> /dev/null \
+    | uuencode -m - \
+    | sed -ne 2p \
+    | cut -c-10)
+
+  three=$(echo "$one$two" | sed 's/./&\n/g' | shuf | tr -d "\n")
+  echo "$three"
+}
