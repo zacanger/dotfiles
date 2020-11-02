1af814668b3e62746a40e570952a4689ca436041
zacanger <zac@zacanger.com>
2020-11-01 18:20:42 -0700

    chore(aliases): poweroff in wsl

--- .bash/aliases/09-misc.sh
+++ .bash/aliases/09-misc.sh
@@ -1,5 +1,10 @@
 # shellcheck shell=bash
 
+# TODO: this isn't tested, but poweroff doesn't work in
+# WSL anyway so it won't hurt anything
+if [[ $(uname -a) == *'microsoft'* ]]; then
+  alias poweroff='powershell.exe "stop-computer"'
+fi
 # this requires sox, and is for DADGAD. change to E2-E4 (etc) for standard.
 alias tuner='for n in D2 A2 D3 G3 A3 D4;do play -n synth 4 pluck $n repeat 3;done'
 alias py='bpython'
