0a33def515b9086c6ba7d409530882e419f2bd8f
zacanger <zac@zacanger.com>
2020-11-18 11:38:57 -0700

    chore(bin): refactor alarm

--- bin/zalarm.sh
+++ bin/zalarm.sh
@@ -1,57 +1,31 @@
 #!/usr/bin/env bash
 set -e
 
-alarmpath=$HOME/Dropbox/z/x/alarm.mp3
+sound() {
+  play -q -n synth 0.05 sin 1000
+}
 
-if ! hash mplayer 2>/dev/null; then
-  echo 'Please install mplayer!'
-  exit 1
-fi
+loop() {
+  for i in {1..4}; do
+    sound
+  done
+  sleep 0.5
+}
 
 if [[ $# -eq 0 ]]; then
   echo "it's now $(date)"
   echo alarm date and time?
-  read -r date
+  read date
 else
-  date="$@"
+  date="$1"
 fi
 
-echo okay! alarm happening at "$(date --date="$date")"
+echo okay! alarm happening at $(date --date="$date")
 
 sleep $(( $(date --date="$date" +%s) - $(date +%s) ))
 
 echo "wake up!"
 
 while true; do
-  $(which mplayer) "$alarmpath"
-  sleep 1
+  loop
 done
-
-# sound() {
-#   play -q -n synth 0.05 sin 1000
-# }
-#
-# loop() {
-#   for i in {1..4}; do
-#     sound
-#   done
-#   sleep 0.5
-# }
-#
-# if [[ $# -eq 0 ]]; then
-#   echo "it's now $(date)"
-#   echo alarm date and time?
-#   read date
-# else
-#   date="$1"
-# fi
-#
-# echo okay! alarm happening at $(date --date="$date")
-#
-# sleep $(( $(date --date="$date" +%s) - $(date +%s) ))
-#
-# echo "wake up!"
-#
-# while true; do
-#   loop
-# done
