8b9e4f987b21e3ef3ea43848860d388cd0b891e0
zacanger <zac@zacanger.com>
2022-06-29 10:10:10 -0600

    fix(shell): brew sbin path

--- .bash/path.sh
+++ .bash/path.sh
@@ -7,7 +7,7 @@ if [[ $(uname) == 'Darwin' ]]; then
     # intell brew stuff is at /usr/local, silicon is at /opt
     export PATH="/usr/local/opt/coreutils/libexec/gnubin:/opt/local/bin:/opt/local/sbin:$PATH"
     export PATH="/usr/local/sbin:$HOME/.local/bin:/usr/local/opt/gettext/bin:$PATH"
-    export PATH="$HOME/.cargo/bin:/opt/homebrew/bin:$PATH"
+    export PATH="$HOME/.cargo/bin:/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
     export PATH="/opt/homebrew/opt/curl/bin:$PATH" # brew link --force doesn't work for this
     export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
     export PATH="$GOPATH/bin:$PATH"
