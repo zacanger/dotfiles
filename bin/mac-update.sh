bd59889aec75620a02997abfbf84fcdb32cf42cd
zacanger <zac@zacanger.com>
2022-07-25 13:18:56 -0600

    chore: misc updates

--- bin/mac-update.sh
+++ bin/mac-update.sh
@@ -7,7 +7,7 @@ brew cleanup --prune=all
 brew doctor --verbose
 
 cat ~/Dropbox/z/misc/npm.list | xargs npm i -g
-cat ~/Dropbox/z/misc/pip.list | sed '/pip/d' | xargs sudo pip3 install -U
+cat ~/Dropbox/z/misc/pip.list | sed '/pip/d' | xargs pip3 install -U
 
 vim +PlugUpgrade +qa
 vim +PlugUpdate +qa
