ff6b8fd8f9ad4f6f1cb3eddc3a58aaa082b70f1c
zacanger <zac@zacanger.com>
2022-08-20 23:16:48 -0600

    feat: bookmarklets

---
+++ misc/bookmarklets/endcards.js
@@ -0,0 +1,2 @@
+// remove endcards on yt vids
+javascript:[...document.getElementsByClassName("ytp-ce-element")].forEach(x => x.style.display = 'none');
---
+++ misc/bookmarklets/shorts.js
@@ -0,0 +1,2 @@
+// switch from a youtube short to a regular video
+javascript:u=window.location.href;n=u.replace('/shorts/','/watch?v=');window.location=n;
