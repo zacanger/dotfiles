b55e91812e7c24a61438c7ccbc83c8b3a2caf529
zacanger <zac@zacanger.com>
2022-08-21 21:23:20 -0600

    chore: bmlet

---
+++ misc/bookmarklets/README.md
@@ -0,0 +1,3 @@
+I put these on my browser bookmark toolbar for easy access. Remove the
+comments if you're going to use them. They also work fine in the browser
+console (remove the `javascript:` part).
---
+++ misc/bookmarklets/nitter.js
@@ -0,0 +1,3 @@
+// switch from twitter, where i don't have an account, to nitter, where i
+// don't need one
+javascript:a=window.location.href;b=a.replace('https://twitter.com/','https://nitter.ca/');window.location=b;
