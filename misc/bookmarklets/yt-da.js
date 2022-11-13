9ddfa4053bd1ebcb227e1d094151b446381b46fa
zacanger <zac@zacanger.com>
2022-11-12 22:50:21 -0700

    chore: remove dead bmlets

--- misc/bookmarklets/yt-da.js
+++
@@ -1,2 +0,0 @@
-// sort youtube videos by date ascending, for channels that have this removed
-javascript:l=window.location.href;a='?view=0&sort=da&flow=grid';window.location=a+l;
--- misc/bookmarklets/yt-p.js
+++
@@ -1,2 +0,0 @@
-// sort youtube videos by popularity, for channels that have this removed
-javascript:l=window.location.href;a='?view=0&sort=p&flow=grid';window.location=a+l;
