897becff1b730d5860cb83c5a809dfaeeb64079a
zacanger <zac@zacanger.com>
2023-02-27 11:16:43 -0700

    chore: update mac-clean

--- bin/mac-clean.sh
+++ bin/mac-clean.sh
@@ -3,20 +3,26 @@
 # Your terminal program will need full disk access
 # for this to really work.
 
+# see also
+# https://github.com/mac-cleanup/mac-cleanup-py/blob/main/mac_cleanup/default_modules.py
+
 sudo -v
 
 del_paths=(
     "$HOME/.Trash/*"
     "$HOME/.cache/youtube-dl/*"
+    "$HOME/.cache/yt-dlp/*"
     "$HOME/.local/share/ranger/*"
     "$HOME/.viminfo"
     "$HOME/.z-trash/*"
     "$HOME/Library/Caches/*"
+    "$HOME/Library/Logs/*"
     /Library/Caches/*
-    /Library/Logs/DiagnosticReports/*
+    /Library/Logs/*
     /System/Library/Caches/*
     /cores/core.*
-    /private/var/log/asl/*.asl
+    /var/log/*
+    /var/logs/*
     /var/spool/cups/c0*
     /var/spool/cups/cache/job.cache*
     /var/spool/cups/tmp/*
@@ -32,6 +38,11 @@ for p in "${del_paths[@]}"; do
     sudo rm -rfv "$p"
 done
 
+for x in $(ls ~/Library/Containers/); do
+    sudo rm -rfv "$HOME/Library/Containers/$x/Data/Library/Caches/*"
+    sudo rm -rfv "$HOME/Library/Containers/$x/Data/Library/Logs/*"
+done
+
 gfind "$HOME" -name '.DS_Store' -type f -delete
 gfind "$HOME" -maxdepth 1 -name '*hist*' -delete
 gfind "$HOME" -maxdepth 1 -name '*hst' -delete
@@ -44,6 +55,7 @@ brew cleanup --prune=all
 gem cleanup
 if [[ "$1" == "-d" ]]; then docker-clear.sh; fi
 npm cache clear -f
+rm -rfv "$HOME/.npm"
 clear-dns-cache.sh
 
 sudo purge
