e719fa2ce5662cc95649d9c6b503b90ff8eb570c
zacanger <zac@zacanger.com>
2023-08-13 16:40:07 -0600

    fix: ninit 2 spaces in json

--- bin/ninit.sh
+++ bin/ninit.sh
@@ -14,72 +14,72 @@ cp "$ND/.npmrc" .npmrc
 cp -R "$ND/.github" .github
 
 WITHOUTG="{
-    \"name\": \"$DN\",
-    \"description\": \"$DN\",
-    \"version\": \"0.0.1\",
-    \"author\": {
-        \"name\": \"Zac Anger\",
-        \"email\": \"zac@zacanger.com\",
-        \"url\": \"https://zacanger.com\"
-    },
-    \"license\": \"MIT\",
-    \"main\": \"index.js\",
-    \"scripts\": {
-        \"test\": \"npm run test:lint && npm run test:tape\",
-        \"test:tape\": \"tape test.js\",
-        \"test:lint\": \"standard\",
-        \"preversion\": \"npm t && sortpack\"
-    },
-    \"engines\": {
-        \"node\": \">=16.0.0\"
-    },
-    \"homepage\": \"https://github.com/zacanger/$DN#readme\",
-    \"repository\": {
-        \"type\": \"git\",
-        \"url\": \"https://github.com/zacanger/$DN.git\"
-    },
-    \"bugs\": \"https://github.com/zacanger/$DN/issues\",
-    \"keywords\": [
-        \"$DN\",
-        \"\"
-    ],
-    \"dependencies\": {},
-    \"devDependencies\": {}
+  \"name\": \"$DN\",
+  \"description\": \"$DN\",
+  \"version\": \"0.0.1\",
+  \"author\": {
+    \"name\": \"Zac Anger\",
+    \"email\": \"zac@zacanger.com\",
+    \"url\": \"https://zacanger.com\"
+  },
+  \"license\": \"MIT\",
+  \"main\": \"index.js\",
+  \"scripts\": {
+    \"test\": \"npm run test:lint && npm run test:tape\",
+    \"test:tape\": \"tape test.js\",
+    \"test:lint\": \"standard\",
+    \"preversion\": \"npm t && sortpack\"
+  },
+  \"engines\": {
+    \"node\": \">=18.0.0\"
+  },
+  \"homepage\": \"https://github.com/zacanger/$DN#readme\",
+  \"repository\": {
+    \"type\": \"git\",
+    \"url\": \"https://github.com/zacanger/$DN.git\"
+  },
+  \"bugs\": \"https://github.com/zacanger/$DN/issues\",
+  \"keywords\": [
+    \"$DN\",
+    \"\"
+  ],
+  \"dependencies\": {},
+  \"devDependencies\": {}
 }"
 
 WITHG="{
-    \"name\": \"$DN\",
-    \"description\": \"$DN\",
-    \"version\": \"0.0.1\",
-    \"author\": {
-        \"name\": \"Zac Anger\",
-        \"email\": \"zac@zacanger.com\",
-        \"url\": \"https://zacanger.com\"
-    },
-    \"license\": \"MIT\",
-    \"main\": \"index.js\",
-    \"scripts\": {
-        \"test\": \"npm run test:lint && npm run test:tape\",
-        \"test:tape\": \"tape test.js\",
-        \"test:lint\": \"standard\",
-        \"preversion\": \"npm t && sortpack\"
-    },
-    \"engines\": {
-        \"node\": \">=16.0.0\"
-    },
-    \"bin\": \"./index.js\",
-    \"homepage\": \"https://github.com/zacanger/$DN#readme\",
-    \"repository\": {
-        \"type\": \"git\",
-        \"url\": \"https://github.com/zacanger/$DN.git\"
-    },
-    \"bugs\": \"https://github.com/zacanger/$DN/issues\",
-    \"keywords\": [
-        \"$DN\",
-        \"\"
-    ],
-    \"dependencies\": {},
-    \"devDependencies\": {}
+  \"name\": \"$DN\",
+  \"description\": \"$DN\",
+  \"version\": \"0.0.1\",
+  \"author\": {
+    \"name\": \"Zac Anger\",
+    \"email\": \"zac@zacanger.com\",
+    \"url\": \"https://zacanger.com\"
+  },
+  \"license\": \"MIT\",
+  \"main\": \"index.js\",
+  \"scripts\": {
+    \"test\": \"npm run test:lint && npm run test:tape\",
+    \"test:tape\": \"tape test.js\",
+    \"test:lint\": \"standard\",
+    \"preversion\": \"npm t && sortpack\"
+  },
+  \"engines\": {
+    \"node\": \">=18.0.0\"
+  },
+  \"bin\": \"./index.js\",
+  \"homepage\": \"https://github.com/zacanger/$DN#readme\",
+  \"repository\": {
+    \"type\": \"git\",
+    \"url\": \"https://github.com/zacanger/$DN.git\"
+  },
+  \"bugs\": \"https://github.com/zacanger/$DN/issues\",
+  \"keywords\": [
+    \"$DN\",
+    \"\"
+  ],
+  \"dependencies\": {},
+  \"devDependencies\": {}
 }"
 
 echo "# $DN
