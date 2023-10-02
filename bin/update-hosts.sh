c7502c5a2f084f92da8e35d2198d3f14fb0a2afb
zacanger <zac@zacanger.com>
2023-10-01 18:07:07 -0600

    chore(hosts): new sources

--- bin/update-hosts.sh
+++ bin/update-hosts.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+# Others to look into
+# (maybe need to just switch to dnsmasq...)
+# https://firebog.net/
+# https://github.com/nextdns/blocklists
 # this is too large, it breaks dns entirely....
 # https://github.com/mitchellkrogza/Ultimate.Hosts.Blacklist
 # curl \
@@ -14,15 +18,30 @@ curl \
     https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn/hosts \
     > hosts2
 
-# cat hosts1 hosts2 /etc/hosts \
-cat hosts2 /etc/hosts \
+# https://github.com/hagezi/dns-blocklists
+# curl \
+    # -sSL \
+    # https://raw.githubusercontent.com/hagezi/dns-blocklists/main/hosts/pro.txt \
+    # > hosts3
+
+curl \
+    -sSL \
+    https://raw.githubusercontent.com/hagezi/dns-blocklists/main/hosts/doh.txt \
+    > hosts4
+
+# curl \
+    # -sSL \
+    # https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/tif.txt \
+    # > hosts5
+
+# cat hosts1 hosts2 hosts3 hosts4 hosts5 /etc/hosts \
+cat hosts2 hosts4 /etc/hosts \
     | sed -e '/^[ \t]*#/d' \
     | sort -u > hosts
 
 echo "0.0.0.0 $(hostname)" >> hosts
 
-# rm hosts1 hosts2
-rm hosts2
+rm hosts2 hosts4
 sudo mv /etc/hosts /etc/hosts.bak
 sudo mv hosts /etc/hosts
 clear-dns-cache.sh
