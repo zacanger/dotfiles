3fd0bbfb415e05057ceb03a83a671d265b4bed37
zacanger <zac@zacanger.com>
2023-10-13 18:19:13 -0600

    fix: override vim editor for remote workspaces

--- .bash/vars.sh
+++ .bash/vars.sh
@@ -12,6 +12,7 @@ export LESSOPEN="| $(which highlight) %s --out-format xterm256 --line-numbers --
 export LESS=" -R"
 # colored GCC warnings and errors
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+export GIT_EDITOR=vim
 
 if [[ $(uname) == 'Darwin' ]]; then
     export MANPATH=/usr/local/opt/coreutils/libexec/gnuman:$MANPATH
