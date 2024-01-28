0f7ffccf6c1fd8b58b0307560ed100546959c91e
zacanger <zac@zacanger.com>
2024-01-28 01:44:27 -0700

    chore: nitter is dead

---
+++ misc/bookmarklets/farside.js
@@ -0,0 +1,2 @@
+// switch from main service to alt service, like nitter, searx, etc
+javascript:a=window.location;b=a.href.replace(a.protocol + '//', 'https://farside.link/_/');window.location=b;
--- misc/bookmarklets/nitter.js
+++
@@ -1,3 +0,0 @@
-// switch from twitter, where i don't have an account, to nitter, where i
-// don't need one
-javascript:a=window.location.href;b=a.replace('https://twitter.com/','https://nitter.net/');window.location=b;
