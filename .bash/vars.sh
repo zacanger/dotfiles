863bf14054e96736a87e38710112f654000a4865
zacanger <zac@zacanger.com>
2024-02-01 10:24:49 -0700

    chore: var

--- .bash/vars.sh
+++ .bash/vars.sh
@@ -13,6 +13,7 @@ export LESS=" -R"
 # colored GCC warnings and errors
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 export GIT_EDITOR=vim
+export GITHUB_USERNAME=zacanger
 
 if [[ $(uname) == 'Darwin' ]]; then
     export MANPATH=/usr/local/opt/coreutils/libexec/gnuman:$MANPATH
