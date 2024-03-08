2de24e3d3f9cd6cccc401402bb0decdc77f14535
zacanger <zac@zacanger.com>
2024-03-08 13:58:47 -0700

    chore: add new keyboard

--- bin/mac-switch-keyboard.sh
+++ bin/mac-switch-keyboard.sh
@@ -2,7 +2,8 @@
 set -e
 
 usage() {
-    echo 'Usage: mac-switch-keyboard.sh [atom|internal]'
+    echo 'Usage: mac-switch-keyboard.sh [keeb]'
+    echo 'Keebs: atom|internal|ada|reset'
     exit 1
 }
 
@@ -14,6 +15,10 @@ if [ "$1" == 'atom' ]; then
     launchctl load -w "$HOME/Library/LaunchAgents/com.local.KeyRemapping.plist"
 elif [ "$1" == 'internal' ]; then
     launchctl unload -w "$HOME/Library/LaunchAgents/com.local.KeyRemapping.plist"
+elif  [ "$1" == 'ada' ]; then
+    /usr/bin/hidutil property --set '{"UserKeyMapping":[ { "HIDKeyboardModifierMappingSrc": 0x700000065, "HIDKeyboardModifierMappingDst": 0x7000000E7 } ]}'
+elif [ "$1" == 'reset' ]; then
+    hidutil property --set '{"UserKeyMapping":[]}'
 else
     usage
 fi
