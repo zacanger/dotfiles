24cdf95beccedbe97d97ede2a8fbbb95d1be3787
zacanger <zac@zacanger.com>
2024-03-22 01:18:02 -0600

    chore: clean up mac-update

--- bin/mac-update.sh
+++ bin/mac-update.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
 brew update
-brew upgrade
 brew upgrade --cask
+brew upgrade
 brew cleanup --prune=all
-brew doctor --verbose
 
 cat ~/Dropbox/z/misc/npm.list | xargs npm i -g
-# TODO: figure out how to get brew and mac to just use one version already
-cat ~/Dropbox/z/misc/pip.list | sed '/pip/d' | xargs pip3 install -U
-cat ~/Dropbox/z/misc/pip.list | sed '/pip/d' | xargs python3 -m pip install -U
 cat ~/Dropbox/z/misc/pip.list | sed '/pip/d' | xargs python3.9 -m pip install -U
 cat ~/Dropbox/z/misc/pip.list | sed '/pip/d' | xargs python3.10 -m pip install -U
-cat ~/Dropbox/z/misc/pip.list | sed '/pip/d' | xargs python3.12 -m pip install -U
+cat ~/Dropbox/z/misc/pip.list | sed '/pip/d' | xargs python3.12 -m pip install -U --break-system-packages
 
 vim +PlugUpgrade +qa
 vim +PlugUpdate +qa
 
-~/bin/update-dircolors.sh
-~/bin/update-hosts.sh
+# Only as needed
+# brew doctor --verbose
+# ~/bin/update-dircolors.sh
+# ~/bin/update-hosts.sh
