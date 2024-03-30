26219091360536bda8527306eaa1388d0c84a2b9
zacanger <zac@zacanger.com>
2024-03-29 20:45:59 -0600

    chore(bin): slow script

--- bin/mac-update.sh
+++ bin/mac-update.sh
@@ -5,7 +5,7 @@ brew upgrade --cask
 brew upgrade
 brew cleanup --prune=all
 
-cat ~/Dropbox/z/misc/npm.list | xargs npm i -g
+for p in $(cat ~/Dropbox/z/misc/npm.list); do npm i -g "$p"; done
 cat ~/Dropbox/z/misc/pip.list | sed '/pip/d' | xargs python3.9 -m pip install -U
 cat ~/Dropbox/z/misc/pip.list | sed '/pip/d' | xargs python3.10 -m pip install -U
 cat ~/Dropbox/z/misc/pip.list | sed '/pip/d' | xargs python3.12 -m pip install -U --break-system-packages
