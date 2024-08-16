2de2e28328e6d3af53f8b4221ede4bcf643166d5
zacanger <zac@zacanger.com>
2024-08-16 00:38:58 -0600

    chore: update bmlets

--- misc/bookmarklets/farside.js
+++
@@ -1,2 +0,0 @@
-// switch from main service to alt service, like nitter, searx, etc
-javascript:a=window.location;b=a.href.replace(a.protocol + '//', 'https://farside.link/_/');window.location=b;
---
+++ misc/bookmarklets/x.js
@@ -0,0 +1,1 @@
+javascript:a=window.location.href;b=a.replace('https://x.com', 'https://xcancel.com');window.location=b;
