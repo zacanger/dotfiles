8b0fcba2f7d4a44292b145abea459445be51e255
zacanger <zac@zacanger.com>
2024-10-20 13:14:18 -0600

    chore: update

--- bin/mac-update.sh
+++ bin/mac-update.sh
@@ -9,6 +9,7 @@ for p in $(cat ~/Dropbox/z/misc/npm.list); do npm i -g "$p"; done
 cat ~/Dropbox/z/misc/pip.list | sed '/pip/d' | xargs python3.9 -m pip install -U
 cat ~/Dropbox/z/misc/pip.list | sed '/pip/d' | xargs python3.10 -m pip install -U
 cat ~/Dropbox/z/misc/pip.list | sed '/pip/d' | xargs python3.12 -m pip install -U --break-system-packages
+cat ~/Dropbox/z/misc/pip.list | sed '/pip/d' | xargs python3.13 -m pip install -U --break-system-packages
 
 vim +PlugUpgrade +qa
 vim +PlugUpdate +qa
